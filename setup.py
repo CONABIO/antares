d5eb45a818bcd08afe3e1b34cbd0f0b6a0703dfe
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-06-09 22:34:22 +0000

    Added script for launching madmex.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@19 df9ad718-c9dd-4117-8c4c-257265001aed

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
 
-config = {
-    'description': 'Measure Activity Data Mexico',
-    'author': 'CONABIO',
-    'url': 'http://www.conabio.gob.mx/',
-    'download_url': 'http://172.16.15.24:8080/job/Madmex/',
-    'author_email': 'agutierrez@conabio.gob.mx',
-    'version': '0.1',
-    'install_requires': ['nose', 'SQLAlchemy'],
-    'packages'=find_packages(),
-    'scripts': [],
-    'name': 'madmex'
-}
 
-setup(**config)
+setup(name='madmex',
+  version='1.0',
+  description='Measure Activity Data Mexico',
+  author='CONABIO',
+  author_email='agutierrez@conabio.gob.mx',
+  url='http://www.conabio.gob.mx/',
+  packages=find_packages(),
+  scripts=['madmex/bin/madmex'],
+  zip_safe = False,
+ )

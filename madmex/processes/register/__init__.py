0930dbb50818ad11279ab259546ccb3ebaa99b18
Erick Palacios <epalacios@conabio.gob.mx>
2015-06-17 20:42:17 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@69 df9ad718-c9dd-4117-8c4c-257265001aed

---
+++ madmex/processes/register/__init__.py
@@ -0,0 +1,27 @@
+'''
+Created on 10/06/2015
+
+@author: erickpalacios
+'''
+from madmex.processes.base import Processes
+
+class Command(Processes):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, name):
+        '''
+        Constructor
+        '''
+        self.name_sensor = name
+    def execute(self):
+        '''
+        Executer
+        '''
+    def __str__(self):
+        '''
+        str
+        '''
+        return "nombre de sensor a registrar: %s" % (self.name_sensor)
+    

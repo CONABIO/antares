3eb14a2f4e8b7803b4145e12655a0e59a3be91bc
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-06-17 16:01:24 +0000

    Added comments to methods.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@46 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/__init__.py
+++ madmex/__init__.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals
 
 import gettext
 from importlib import import_module
-import locale
 import logging
 import os
 import pkgutil
@@ -16,21 +15,22 @@ from madmex.core.log import setup_logging
 __version__ = '2.1'
 os.environ['LANGUAGE'] = 'es_MX'
 
-locale_path = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'locale'
-        )
+LOCALE_PATH = os.path.join(
+              os.path.dirname(os.path.realpath(__file__)),
+              'locale'
+    )
 
-print locale_path
+LANGUAGE = gettext.translation ('madmex', LOCALE_PATH)
+LANGUAGE.install()
+_ = LANGUAGE.ugettext
 
-language = gettext.translation ('madmex', locale_path)
-language.install()
-_ = language.ugettext
-
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 def setup():
+    '''
+    Calls method that sets up the configuration of the logging framework.
+    '''
     setup_logging()
 
 def get_version():
@@ -40,10 +40,21 @@ def get_version():
     return __version__
 
 def find_in_dir(path, package):
+    '''
+    Given a path to a directory, returns a list of all the module names
+    that are available.
+
+    Returns an empty list if no commands are defined.
+    '''
     directory = os.path.join(path, package)
     return [name for _, name, is_pkg in pkgutil.iter_modules([directory])
             if not is_pkg and not name.startswith('_')]
     
-def load_class(package,name):
+def load_class(package, name):
+    '''
+    Given a package and a name, it returns an instance of the class that is
+    named like that. All errors raised by the import process
+    (ImportError, AttributeError) are allowed to propagate.
+    '''
     module = import_module('%s.%s' % (package, name))
     return module

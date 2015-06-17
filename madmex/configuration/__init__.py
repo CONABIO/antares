36525f2856164da264eaa24ee6baaa6860c0aacc
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-06-17 16:11:46 +0000

    Fixed code style errors.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@51 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/configuration/__init__.py
+++ madmex/configuration/__init__.py
@@ -12,14 +12,14 @@ import os
 
 ENVIRONMENT_VARIABLE = "MADMEX_SETTINGS_MODULE"
 
-def set_settings_from_configuration(SETTINGS, parser):
+def set_settings_from_configuration(settings, parser):
     '''
     Reads a configuration file, it traverses the sections taking the (key,value)
-    pairs and setting them as the SETTINGS object attributes. 
+    pairs and setting them as the SETTINGS object attributes.
     '''
     for section in parser.sections():
         for option in parser.options(section):
-            setattr(SETTINGS, option.upper(), parser.get(section, option))
+            setattr(settings, option.upper(), parser.get(section, option))
 
 class Settings(object):
     '''
@@ -31,15 +31,15 @@ class Settings(object):
         '''
         constructor
         '''
-        self.load()        
+        self.load()
     def load(self):
         '''
         Protected method to read the file for the first time.
         '''
         settings_file = os.environ.get(ENVIRONMENT_VARIABLE)
         path = os.path.join(
-               os.path.dirname(os.path.realpath(__file__)),
-               'configuration.ini'
+                            os.path.dirname(os.path.realpath(__file__)),
+                            'configuration.ini'
         )
         if settings_file and os.path.isfile(settings_file):
             settings_files = [path, settings_file]
@@ -47,7 +47,7 @@ class Settings(object):
             settings_files = [path]
         setattr(self, 'SETTINGS_FILES', settings_files)
         parser = ConfigParser.SafeConfigParser()
-        # TODO: fail gracefully when no header is detected at the beginning. 
+        # TODO: fail gracefully when no header is detected at the beginning.
         parser.read(settings_files)
 
         set_settings_from_configuration(self, parser) 

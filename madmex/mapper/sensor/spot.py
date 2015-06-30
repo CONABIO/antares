af49a92a72c1cbff422d122c9c7661f2d6966815
Erick Palacios <epalacios@conabio.gob.mx>
2015-06-30 22:49:45 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@113 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/sensor/spot.py
+++ madmex/mapper/sensor/spot.py
@@ -26,28 +26,26 @@ class Sensor(BaseSensor):
                     "tileid" : ["Dimap_Document","Dataset_Sources","Scene_Source","GRID_REFERENCE"]
                     }
         self.metadata=dict()
-    def change_format(self, metadataStruct):
+    def change_format(self):
         try:
  
-            self.metadataStruct = metadataStruct
-            acquisitionDate = self.metadataStruct["dataAcquisition"]
-            acquisitionTime = self.metadataStruct["dataAcquisitionTime"]
-            creationDate = self.metadataStruct["dateCreation"]
-            self.metadataStruct["dataAcquisition"] = datetime.strptime(acquisitionDate+"T"+acquisitionTime, "%Y-%m-%dT%H:%M:%S")
+            acquisitionDate = self.metadata["dataAcquisition"]
+            acquisitionTime = self.metadata["dataAcquisitionTime"]
+            creationDate = self.metadata["dateCreation"]
+            self.metadata["dataAcquisition"] = datetime.strptime(acquisitionDate+"T"+acquisitionTime, "%Y-%m-%dT%H:%M:%S")
             
-            self.metadataStruct["dateCreation"] = datetime.strptime(creationDate, "%Y-%m-%dT%H:%M:%S.%f")
-            self.metadataStruct["angle"] = float(self.metadataStruct["angle"])
+            self.metadata["dateCreation"] = datetime.strptime(creationDate, "%Y-%m-%dT%H:%M:%S.%f")
+            self.metadata["angle"] = float(self.metadata["angle"])
             
-            self.metadataStruct["clouds"] = 999
-            self.metadataStruct["quicklook"] = "PREVIEW.JPG"
-            self.sensorname = self.metadataStruct["sensor"]
-            self.productname = self.metadataStruct["product"]
-            self.dateCreation = self.metadataStruct["dateCreation"]
-            self.dataAcquisition = self.metadataStruct["dataAcquisition"]
-            self.clouds = self.metadataStruct["clouds"]
-            self.angle = self.metadataStruct["angle"]
-            self.quicklookUrl = self.metadataStruct["quicklook"]
-            self.gridid = int(self.metadataStruct["tileid"])                   
+            self.metadata["clouds"] = 999
+            self.metadata["quicklook"] = "PREVIEW.JPG"
+            self.productname = self.metadata["product"]
+            self.dateCreation = self.metadata["dateCreation"]
+            self.dataAcquisition = self.metadata["dataAcquisition"]
+            self.clouds = self.metadata["clouds"]
+            self.angle = self.metadata["angle"]
+            self.quicklookUrl = self.metadata["quicklook"]
+            self.gridid = int(self.metadata["tileid"])                   
         except IOError:
             raise "Meta data extraction not successful"
         

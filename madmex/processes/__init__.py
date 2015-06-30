20486a7fffa0d4bbbbe18117678d8ef71dd4f483
Erick Palacios <epalacios@conabio.gob.mx>
2015-06-30 20:11:11 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@101 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/processes/__init__.py
+++ madmex/processes/__init__.py
@@ -3,34 +3,25 @@ Created on 10/06/2015
 
 @author: erickpalacios
 '''
-from importlib import import_module
-from madmex import load_class
-from madmex.processes.base import Processes
 
-PROCESSES_PACKAGE = 'madmex.processes'
 class Manager(object):
     '''
     Manager
     '''
-    def __init__(self, process, input_data):
-        self.name_process = process
-        self.input_data = input_data
+    def __init__(self):
+        '''
+        init
+        '''
     def execute(self):
         '''
         Execute
         '''
-        instance_class = self.load_processes_class(PROCESSES_PACKAGE)
-        instance_class.execute()
-        self.output_data = instance_class.output
+
     def execute_parallel(self):
         '''
         execute in parallel some process
         receives a directory with folders within. Instance Process() for each folder separately
         '''
-    def load_processes_class(self, package):
-        '''
-        load_processes_class
-        '''
-        module = load_class(package, self.name_process)
-        return module.Process(self.input_data)
- 
+
+
+

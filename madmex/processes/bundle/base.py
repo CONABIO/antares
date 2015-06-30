8466973e2069193fa1d38222d9537ac1443f971f
Erick Palacios <epalacios@conabio.gob.mx>
2015-06-30 20:12:26 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@103 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/processes/bundle/base.py
+++ madmex/processes/bundle/base.py
@@ -3,21 +3,31 @@ Created on 18/06/2015
 
 @author: erickpalacios
 '''
-class Basebundle(object):
+class BaseBundle(object):
     '''
     This class serves as a base shell for a bundle. A bundle is a set of files
     that represent a working piece of information. The implementation of this
     class is in charge of looking for the needed files and throw an error in
     case any of the given files is missing or is incorrect.
     '''
-
-    def scan(self):
+    def scan(self, list_ref, list_test):
         '''
-        This method will traverse through the list of files in the given
-        directory using the given regex dictionary, creating a map for the
-        founded files.
+        This method test whether the elements of list_test are in list_ref and if success return the index 
+        list_ref is the reference list
+        list_test is the list that we want to test
         '''
-        raise NotImplementedError('subclasses of BaseBundle must provide a scan() method')
+        val = False
+        k = 0
+        while k < len(list_ref) and (not val):
+            try:
+                index = list_test.index(list_ref[k])
+                val = True
+                result = index
+            except ValueError:
+                    k = k+1
+        if k == len(list_ref) and (not val):
+            result = 'not founded'
+        return result
     def is_consistent(self):
         '''
         Subclasses must implement this method.

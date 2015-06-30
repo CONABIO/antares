23085e2a035b72c52370f5fae0ea544bcd8c2ac0
Erick Palacios <epalacios@conabio.gob.mx>
2015-06-30 20:12:56 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@104 df9ad718-c9dd-4117-8c4c-257265001aed

---
+++ madmex/processes/extractimagemetadata/base.py
@@ -0,0 +1,17 @@
+'''
+Created on 30/06/2015
+
+@author: erickpalacios
+'''
+
+class BaseFormat(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ madmex/processes/extractimagemetadata/format/tif.py
@@ -0,0 +1,18 @@
+'''
+Created on 30/06/2015
+
+@author: erickpalacios
+'''
+from madmex.processes.extractimagemetadata.base import BaseFormat
+
+class Format(BaseFormat):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        

f38ccd8bf9e9ed0c09b55a4214bdc018238cd626
Erick Palacios <epalacios@conabio.gob.mx>
2015-07-01 19:55:07 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@119 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/processes/extractsensormetadata/parser/xmlparser.py
+++ madmex/processes/extractsensormetadata/parser/xmlparser.py
@@ -4,39 +4,31 @@ Created on 25/06/2015
 
 @author: erickpalacios
 '''
-from madmex import load_class
 from xml.dom import minidom
 from xml.parsers.expat import ExpatError
 import unicodedata
-
-SENSORS_PACKAGE = 'madmex.mapper.sensor'
 
 class XmlParser(object):
     '''
     classdocs
     '''
 
-    def __init__(self, metadata_path):
+    def __init__(self, metadata_path, tagList):
         '''
         Constructor
         '''
         self.metadata_path=metadata_path
+        self.tagList = tagList
 
-    def run(self):
+    def run(self, metadata):
         print "open", self.metadata_path
         try:
-            datafile = minidom.parse(self.metadata_path)
-            if datafile.getElementsByTagName('Dimap_Document'):
-                sensor_class = load_class(SENSORS_PACKAGE, 'spot').Sensor()
-            else:
-                if datafile.getElementsByTagName('re:EarthObservationMetaData'):
-                    sensor_class = load_class(SENSORS_PACKAGE, 'rapideye').Sensor()            
-            for i in sensor_class.tagList.keys():
-                len_tagList = len(sensor_class.tagList[str(i)])
-                elem = datafile.getElementsByTagName(sensor_class.tagList[str(i)][len_tagList-1])
-                sensor_class.metadata[str(i)] = unicodedata.normalize('NFKD', elem[0].firstChild.nodeValue).encode('ascii','ignore')
-            sensor_class.change_format()
-            return sensor_class
+            datafile = minidom.parse(self.metadata_path)          
+            for i in self.tagList.keys():
+                len_tagList = len(self.tagList[str(i)])
+                elem = datafile.getElementsByTagName(self.tagList[str(i)][len_tagList-1])
+                metadata[str(i)] = unicodedata.normalize('NFKD', elem[0].firstChild.nodeValue).encode('ascii','ignore')
+            return metadata
         except ExpatError:
             print 'error in xml metadata file:%s' % self.metadata_path
             raise

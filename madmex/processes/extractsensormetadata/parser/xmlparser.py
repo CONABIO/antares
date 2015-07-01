ab09084f2bc52090c7bd3ee11c049571e0628a0b
Erick Palacios <epalacios@conabio.gob.mx>
2015-07-01 18:28:13 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@117 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/processes/extractsensormetadata/parser/xmlparser.py
+++ madmex/processes/extractsensormetadata/parser/xmlparser.py
@@ -4,10 +4,9 @@ Created on 25/06/2015
 
 @author: erickpalacios
 '''
+from madmex import load_class
 from xml.dom import minidom
 from xml.parsers.expat import ExpatError
-import xml.sax, csv
-from madmex import load_class
 import unicodedata
 
 SENSORS_PACKAGE = 'madmex.mapper.sensor'
@@ -17,47 +16,27 @@ class XmlParser(object):
     classdocs
     '''
 
-
     def __init__(self, metadata_path):
         '''
         Constructor
         '''
         self.metadata_path=metadata_path
-    def get_type(self, filename):
-        if """<?xml version="1.0"?>""" in filename:
-            return "xml"
-        print "open ",filename
-        with open(filename, 'rb') as fh:
-            try:
-                xml.sax.parse(fh, xml.sax.ContentHandler())
-                return 'xml'
-            except: 
-                pass
-            fh.seek(0)
-            try:
-                for line in csv.reader(fh):
-                    pass
-                return 'csv'
-            except csv.Error:
-                pass
-    
-            return 'txt'
-    
-    def run(self):
-        if self.get_type(self.metadata_path) == "xml":
-            try:
-                datafile = minidom.parseString(self.metadata_path)
 
-            except ExpatError, e:
-                datafile = minidom.parse(self.metadata_path)
+    def run(self):
+        print "open", self.metadata_path
+        try:
+            datafile = minidom.parse(self.metadata_path)
             if datafile.getElementsByTagName('Dimap_Document'):
                 sensor_class = load_class(SENSORS_PACKAGE, 'spot').Sensor()
             else:
                 if datafile.getElementsByTagName('re:EarthObservationMetaData'):
                     sensor_class = load_class(SENSORS_PACKAGE, 'rapideye').Sensor()            
             for i in sensor_class.tagList.keys():
-                long_tagList = len(sensor_class.tagList[str(i)])
-                elem = datafile.getElementsByTagName(sensor_class.tagList[str(i)][long_tagList-1])
+                len_tagList = len(sensor_class.tagList[str(i)])
+                elem = datafile.getElementsByTagName(sensor_class.tagList[str(i)][len_tagList-1])
                 sensor_class.metadata[str(i)] = unicodedata.normalize('NFKD', elem[0].firstChild.nodeValue).encode('ascii','ignore')
             sensor_class.change_format()
             return sensor_class
+        except ExpatError:
+            print 'error in xml metadata file:%s' % self.metadata_path
+            raise

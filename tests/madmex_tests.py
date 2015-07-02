5ce9b334498e2b288427f33e57602745bda61ed7
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-02 21:03:27 +0000

    Added test for the landsat parser.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@141 df9ad718-c9dd-4117-8c4c-257265001aed

--- tests/madmex_tests.py
+++ tests/madmex_tests.py
@@ -10,7 +10,9 @@ import os
 import unittest
 
 from madmex.core import controller
+from madmex.mapper.parser.landsat import Parser
 
+from madmex.configuration import SETTINGS
 
 class Test(unittest.TestCase):
 
@@ -32,7 +34,6 @@ class Test(unittest.TestCase):
     def testName(self):
         pass
 
-
     def test_configuration(self):
         """
         Creates a new configuration file loads it and tests if a setting is
@@ -50,17 +51,26 @@ class Test(unittest.TestCase):
         test_file.write('%s=%s\n' % (key, value))
         test_file.close()
         
-        from madmex.configuration import SETTINGS
         from madmex.configuration import ENVIRONMENT_VARIABLE
         SETTINGS.reload()
         os.environ[ENVIRONMENT_VARIABLE] = path
-        print dir(SETTINGS)
         SETTINGS.reload()
-        print dir(SETTINGS)
         self.assertEqual(value, getattr(SETTINGS, key.upper()))
         del os.environ[ENVIRONMENT_VARIABLE]
         os.remove(path)
 
+    def test_landsat_parser(self):
+        '''
+        Test for the landsat parser, creates a parser instance pointing to a
+        landsat xml file and then two properties are retrieved.
+        '''
+        metadata = "/LUSTRE/MADMEX/eodata/etm+/25046/2013/2013-04-03/l1t/LE70250462013093ASN00_MTL.txt"
+        parser = Parser(metadata)
+        parser.parse()
+        sun_azimuth = parser.get_attribute(['L1_METADATA_FILE','IMAGE_ATTRIBUTES','SUN_AZIMUTH'])
+        scene_start_time = parser.get_attribute(['searchResponse','metaData','sceneStartTime'])
+        self.assertEqual(sun_azimuth, 116.96407755)
+        self.assertEqual(scene_start_time, '2013:093:16:49:15.2943749')
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()

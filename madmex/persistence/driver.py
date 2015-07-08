47b2170f22b960f59eaf8fde3ca20920e0fa0fab
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-08 21:57:42 +0000

    Refactored some methods.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@154 df9ad718-c9dd-4117-8c4c-257265001aed

---
+++ madmex/persistence/driver.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 8, 2015
+
+@author: agutierrez
+'''
+from __future__ import unicode_literals
+
+class PersistenceDriver(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        

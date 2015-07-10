347d5fd6973578cad6dc38d786949c0c0545ecbb
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-10 16:11:25 +0000

    Changes to vector and raster classes.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@160 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/data/raster.py
+++ madmex/mapper/data/raster.py
@@ -3,24 +3,33 @@ Created on 10/06/2015
 
 @author: erickpalacios
 '''
-from madmex.mapper.base import BaseData
+from __future__ import unicode_literals
+
+import logging
+
 import gdal, gdalconst
 import ogr, osr
-from geoalchemy2 import WKTElement
+
+from madmex.mapper.base import BaseData
+
 
 gdal.AllRegister()
 gdal.UseExceptions()
+
+LOGGER = logging.getLogger(__name__)
 
 class Data(BaseData):
     
     def __init__(self, image_path, gdal_format):
         
         self.image_path = image_path
-        self.driver = gdal.GetDriverByName(gdal_format)
-            
+        try:
+            self.driver = gdal.GetDriverByName(gdal_format)
+        except AttributeError:
+            LOGGER.error('Cannot access driver for format %s' % gdal_format)
     def open_file(self, mode = gdalconst.GA_ReadOnly):
         '''
-        Open the raster image file with gdal
+        Open the raster image file with gdal.
         '''
         try:
             return gdal.Open(self.image_path, mode)
@@ -28,18 +37,16 @@ class Data(BaseData):
             print 'Unable to open raster file %s' % self.image_path
     def extract_metadata(self, data):
         '''
-        Extract metadata of the raster image file with gdal functions
+        Extract metadata from the raster image file using gdal functions.
         '''
         self.projection = data.GetProjection()
         self.geotransform = data.GetGeoTransform()
         self.dataShape = (data.RasterXSize, data.RasterYSize, data.RasterCount)
         self.footprint = self.get_footprint()
-            
     def get_footprint(self):
         '''
-        Get the extent of the raster image
+        Returns the extent of the raster image.
         '''
-        
         ring = ogr.Geometry(ogr.wkbLinearRing) 
         ring.AddPoint_2D(self.geotransform[0], self.geotransform[3]) 
         ring.AddPoint_2D(self.geotransform[0] + self.geotransform[1] * self.dataShape[0], self.geotransform[3])  
@@ -47,17 +54,7 @@ class Data(BaseData):
         ring.AddPoint_2D(self.geotransform[0], self.geotransform[3] + self.geotransform[5] * self.dataShape[1]) 
         ring.CloseRings()
         
+        spacial_reference = osr.SpatialReference()
+        spacial_reference.ImportFromWkt(self.projection)
         
-        sourceSR = osr.SpatialReference()
-        sourceSR.ImportFromWkt(self.projection)
-        targetSR = osr.SpatialReference() 
-        targetSR.ImportFromEPSG(4326)  # Geo WGS84
-        coordTrans = osr.CoordinateTransformation(sourceSR, targetSR)
-        
-        footprint = ogr.Geometry(ogr.wkbPolygon)
-        footprint.AddGeometry(ring)
-        footprint.Transform(coordTrans) 
-        wkt = WKTElement(footprint.ExportToWkt(),srid=4326)
-
-        return wkt
-        
+        return self._footprint_helper(ring, spacial_reference)

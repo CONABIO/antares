598e915c702c51fdc303f9cf75d2ecfecba32b46
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-15 22:13:39 +0000

    Fixed pylint errors.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@202 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/bundle/modis.py
+++ madmex/mapper/bundle/modis.py
@@ -4,22 +4,27 @@ Created on Jul 10, 2015
 @author: agutierrez
 '''
 from __future__ import unicode_literals
+
 from madmex.mapper.base import BaseBundle
+
 
 class Bundle(BaseBundle):
     '''
     classdocs
     '''
-
-
     def __init__(self, params):
         '''
         Constructor
         '''
+        super(Bundle, self).__init__()
         pass
-        
     def can_identify(self):
+        '''
+        Test if the parsed path can be identified as a Modis bundle.
+        '''
         return False
-    
     def get_name(self):
+        '''
+        Returns the name of the bundle.
+        '''
         return 'Modis'

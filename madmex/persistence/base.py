7ee124eefe4b1fae1fdfb8cd0ab54260275b8606
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-15 16:58:11 +0000

    Refactor "do" method to "act", it was invalid.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@176 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/persistence/base.py
+++ madmex/persistence/base.py
@@ -5,7 +5,7 @@ Created on Jul 7, 2015
 '''
 from __future__ import unicode_literals
 
-class BaseAction():
+class BaseAction(object):
     '''
     This class represents an action that can be done and then undone in case
     that such a behavior is needed.
@@ -14,13 +14,13 @@ class BaseAction():
         '''
         Constructor
         '''
-    def do(self):
+    def act(self):
         '''
         Performs the action that the implementor represents.
         '''
-        raise NotImplemented('Extending class must implement a do method.')
+        raise NotImplementedError('Extending class must implement a act method.')
     def undo(self):
         '''
         Undoes the action that the implementor represents.
         '''
-        raise NotImplemented('Extending class must implement a undo method.')
+        raise NotImplementedError('Extending class must implement a undo method.')

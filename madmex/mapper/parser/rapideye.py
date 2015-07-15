dd57418ae80992b474cf274a8d12b92f0370587b
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-15 18:27:52 +0000

    Removed trailing whitespace.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@192 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/parser/rapideye.py
+++ madmex/mapper/parser/rapideye.py
@@ -9,16 +9,18 @@ from __future__ import unicode_literals
 from madmex.mapper.base import BaseParser, _xml_to_json
 import xml.dom.minidom as dom
 
-
 class Parser(BaseParser):
     '''
-    classdocs
+    This class implements a parser for RapidEye metadata. RapidEye metadata
+    comes in a simple xml file so we can just call the xml parser to create
+    a in memory representation using a dictionary.
     '''
-
     def parse(self):
+        '''
+        Opens the metadata file and creates a json representation of it.
+        '''
         self.metadata = {}
         document = dom.parse(self.filepath)
         stack = []
         self.metadata = {}
         _xml_to_json(document.documentElement, stack, self.metadata)
-        

74b320009e150caaadb33e58d4524a7d079f07b3
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-15 22:07:04 +0000

    Fixed format problems.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@200 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/sensor/rapideye.py
+++ madmex/mapper/sensor/rapideye.py
@@ -11,18 +11,87 @@ import madmex.mapper.parser.rapideye as rapideye
 
 SENSOR_NAME = ['sensor_name']
 METADATA_EXT = 'xml'
-PRODUCT_NAME = ['re:EarthObservation', 'gml:metaDataProperty', 're:EarthObservationMetaData','eop:productType']
-SENSOR = ['re:EarthObservation', 'gml:using', 'eop:EarthObservationEquipment', 'eop:sensor', 're:Sensor', 'eop:sensorType']
-PLATFORM = ['re:EarthObservation', 'gml:using', 'eop:EarthObservationEquipment', 'eop:platform', 'eop:Platform', 'eop:serialIdentifier']
-CREATION_DATE = ['re:EarthObservation', 'gml:metaDataProperty', 're:EarthObservationMetaData', 'eop:archivedIn', 'eop:ArchivingInformation', 'eop:archivingDate']
-ACQUISITION_DATE = ['re:EarthObservation', 'gml:metaDataProperty', 're:EarthObservationMetaData', 'eop:downlinkedTo', 'eop:DownlinkInformation', 'eop:acquisitionDate']
-ANGLE = ['re:EarthObservation', 'gml:using', 'eop:EarthObservationEquipment', 'eop:acquisitionParameters', 're:Acquisition', 'eop:incidenceAngle']
-CLOUDS = ['re:EarthObservation', 'gml:resultOf', 're:EarthObservationResult', 'opt:cloudCoverPercentage']
+PRODUCT_NAME = ['re:EarthObservation',
+    'gml:metaDataProperty',
+    're:EarthObservationMetaData',
+    'eop:productType']
+SENSOR = [
+    're:EarthObservation',
+    'gml:using',
+    'eop:EarthObservationEquipment',
+    'eop:sensor',
+    're:Sensor',
+    'eop:sensorType'
+    ]
+PLATFORM = [
+    're:EarthObservation',
+    'gml:using',
+    'eop:EarthObservationEquipment',
+    'eop:platform',
+    'eop:Platform',
+    'eop:serialIdentifier'
+    ]
+CREATION_DATE = [
+    're:EarthObservation',
+    'gml:metaDataProperty',
+    're:EarthObservationMetaData',
+    'eop:archivedIn',
+    'eop:ArchivingInformation',
+    'eop:archivingDate'
+    ]
+ACQUISITION_DATE = [
+    're:EarthObservation',
+    'gml:metaDataProperty',
+    're:EarthObservationMetaData',
+    'eop:downlinkedTo',
+    'eop:DownlinkInformation',
+    'eop:acquisitionDate'
+    ]
+ANGLE = [
+    're:EarthObservation',
+    'gml:using',
+    'eop:EarthObservationEquipment',
+    'eop:acquisitionParameters',
+    're:Acquisition',
+    'eop:incidenceAngle'
+    ]
+CLOUDS = [
+    're:EarthObservation',
+    'gml:resultOf',
+    're:EarthObservationResult',
+    'opt:cloudCoverPercentage'
+    ]
 QUICKLOOK = ['quicklook']
-AZIMUTH_ANGLE = ['re:EarthObservation', 'gml:using', 'eop:EarthObservationEquipment', 'eop:acquisitionParameters', 're:Acquisition', 're:azimuthAngle']
-SOLAR_AZIMUTH = ['re:EarthObservation', 'gml:using', 'eop:EarthObservationEquipment', 'eop:acquisitionParameters', 're:Acquisition', 'opt:illuminationAzimuthAngle']
-SOLAR_ZENITH = ['re:EarthObservation', 'gml:using', 'eop:EarthObservationEquipment', 'eop:acquisitionParameters', 're:Acquisition', 'opt:illuminationElevationAngle']
-TILE_ID = ['re:EarthObservation', 'gml:metaDataProperty', 're:EarthObservationMetaData', 're:tileId']
+AZIMUTH_ANGLE = [
+    're:EarthObservation',
+    'gml:using',
+    'eop:EarthObservationEquipment',
+    'eop:acquisitionParameters',
+    're:Acquisition',
+    're:azimuthAngle'
+    ]
+SOLAR_AZIMUTH = [
+    're:EarthObservation',
+    'gml:using',
+    'eop:EarthObservationEquipment',
+    'eop:acquisitionParameters',
+    're:Acquisition',
+    'opt:illuminationAzimuthAngle'
+    ]
+SOLAR_ZENITH = [
+    're:EarthObservation',
+    'gml:using',
+    'eop:EarthObservationEquipment',
+    'eop:acquisitionParameters',
+    're:Acquisition',
+    'opt:illuminationElevationAngle'
+    ]
+TILE_ID = [
+    're:EarthObservation',
+    'gml:metaDataProperty',
+    're:EarthObservationMetaData',
+    're:tileId'
+    ]
 
 class Sensor(BaseSensor):
     '''
@@ -32,5 +101,8 @@ class Sensor(BaseSensor):
         super(Sensor, self).__init__()
         self.parser = rapideye.Parser(metadata_path)
         self.parser.parse()
-        self.parser.apply_format(ACQUISITION_DATE, lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"))
+        self.parser.apply_format(
+            ACQUISITION_DATE, 
+            lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ")
+            )
         self.parser.set_attribute(SENSOR_NAME, 'rapideye')

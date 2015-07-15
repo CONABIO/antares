05ac9d51a1f3393674b7d9fc702f894103e6caff
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-15 21:39:29 +0000

    Fixed reference errors.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@199 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/data/raster.py
+++ madmex/mapper/data/raster.py
@@ -24,8 +24,7 @@ class Data(BaseData):
     inheritance from this class to represent each type of image file, right now
     it is only a helper class to open raster files.
     '''
-    
-    def __init__(self, image_path, gdal_format):
+    def __init__(self, image_path):
         '''
         Constructor
         '''
@@ -33,22 +32,26 @@ class Data(BaseData):
         self.data_file = self._open_file()
         self.metadata = {}
         self._extract_metadata()
-    def _open_file(self, mode = gdalconst.GA_ReadOnly):
+    def _open_file(self, mode=gdalconst.GA_ReadOnly):
         '''
         Open the raster image file with gdal.
         '''
         try:
-            LOGGER.debug('Open raster file: %s' % self.image_path)
+            LOGGER.debug('Open raster file: %s', self.image_path)
             return gdal.Open(self.image_path, mode)
         except RuntimeError:
-            LOGGER.error('Unable to open raster file %s' % self.image_path)
+            LOGGER.error('Unable to open raster file %s', self.image_path)
     def _extract_metadata(self):
         '''
         Extract metadata from the raster image file using gdal functions.
         '''
         self.metadata['projection'] = self.data_file.GetProjection()
         self.metadata['geotransform'] = self.data_file.GetGeoTransform()
-        self.metadata['data_shape'] = (self.data_file.RasterXSize, self.data_file.RasterYSize, self.data_file.RasterCount)
+        self.metadata['data_shape'] = (
+            self.data_file.RasterXSize,
+            self.data_file.RasterYSize,
+            self.data_file.RasterCount
+            )
         self.metadata['footprint'] = self._get_footprint()
     def _get_footprint(self):
         '''
@@ -57,13 +60,20 @@ class Data(BaseData):
         ring = ogr.Geometry(ogr.wkbLinearRing) 
         geotransform = self.get_attribute('geotransform')
         data_shape = self.get_attribute('data_shape')
-        ring.AddPoint_2D(geotransform[0], geotransform[3]) 
-        ring.AddPoint_2D(geotransform[0] + geotransform[1] * data_shape[0], geotransform[3])  
-        ring.AddPoint_2D(geotransform[0] + geotransform[1] * data_shape[0], geotransform[3] + geotransform[5] * data_shape[1])  
-        ring.AddPoint_2D(geotransform[0], geotransform[3] + geotransform[5] * data_shape[1]) 
+        ring.AddPoint_2D(geotransform[0], geotransform[3])
+        ring.AddPoint_2D(geotransform[0] + geotransform[1] * data_shape[0], geotransform[3])
+        ring.AddPoint_2D(
+            geotransform[0] + geotransform[1] * data_shape[0],
+            geotransform[3] + geotransform[5] * data_shape[1]
+            )
+        ring.AddPoint_2D(geotransform[0], geotransform[3] + geotransform[5] * data_shape[1])
         ring.CloseRings()    
         spacial_reference = osr.SpatialReference()
         spacial_reference.ImportFromWkt(self.get_attribute('projection'))
         return self._footprint_helper(ring, spacial_reference)
     def get_attribute(self, key):
+        '''
+        Returns the attribute that is found with the given key in the metadata
+        dictionary.
+        '''
         return _get_attribute([key], self.metadata)

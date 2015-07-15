69d093d09db7aa8201f42ad58e3edb42ec11098f
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-07-15 22:10:26 +0000

    Fixed format errors.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@201 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/data/vector.py
+++ madmex/mapper/data/vector.py
@@ -16,13 +16,18 @@ ogr.UseExceptions()
 LOGGER = logging.getLogger(__name__)
 
 class Data(BaseData):
-    
+    '''
+    This class represents a vector type file.
+    '''
     def __init__(self, image_path, ogr_format):
+        super(Data, self).__init__()
         self.image_path = image_path
+        self.footprint = None
+        self.layer = None
         try:
             self.driver = ogr.GetDriverByName(ogr_format)
         except AttributeError:
-            LOGGER.error('Cannot access driver for format %s' % ogr_format)
+            LOGGER.error('Cannot access driver for format %s', ogr_format)
     def _open_file(self):
         '''
         Open the vector image file with ogr.
@@ -30,20 +35,20 @@ class Data(BaseData):
         try:
             return self.driver.Open(self.image_path, 0) # 0 means read-only. 1 means writeable.
         except Exception:
-            LOGGER.error('Unable to open shape file: %s' % self.image_path)
+            LOGGER.error('Unable to open shape file: %s', self.image_path)
     def _extract_metadata(self, data):
         '''
         Extract metadata from the raster image file using gdal functions.
         '''
         self.layer = data.GetLayer()
-        self.footprint = self.get_footprint()
+        self.footprint = self._get_footprint()
     def _get_footprint(self):
         '''
         Returns the extent of the shape image.
         '''
         extent = self.layer.GetExtent()
-        ring = ogr.Geometry(ogr.wkbLinearRing) 
-        ring.AddPoint_2D(extent[0],extent[2])
+        ring = ogr.Geometry(ogr.wkbLinearRing)
+        ring.AddPoint_2D(extent[0], extent[2])
         ring.AddPoint_2D(extent[1], extent[2])
         ring.AddPoint_2D(extent[1], extent[3])
         ring.AddPoint_2D(extent[0], extent[3])

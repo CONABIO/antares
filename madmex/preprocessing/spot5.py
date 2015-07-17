b1b3fb3fd5ca46db0948bfad6a87b8d729029afc
Erick Palacios <epalacios@conabio.gob.mx>
2015-07-17 17:57:45 +0000

    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@236 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/preprocessing/spot5.py
+++ madmex/preprocessing/spot5.py
@@ -13,14 +13,15 @@ def Spot5DN2TOA(indir):
     print "Start folder: ", indir
     bundle = spot5.Bundle(indir)
     if bundle.can_identify():
-        bundle.get_raster()
+        #bundle.get_raster()
         bundle.get_sensor()
-        bundle.calculate_toa()
+        #bundle.calculate_toa()
+        bundle.export()
     
     
 
 if __name__ == '__main__':
     
-    #folder = '/Volumes/Imagenes_originales/SPOT5/SPOTMarz/SinNubes/E55542961503031J1A02002/SCENE01'
-    folder = '/Users/erickpalacios/Documents/CONABIO/Tareas/Tarea11/spot5/E55542961503031J1A02002/SCENE01'
+    folder = '/Volumes/Imagenes_originales/SPOT5/SPOTMarz/SinNubes/E55542961503031J1A02002/SCENE01'
+    #folder = '/Users/erickpalacios/Documents/CONABIO/Tareas/Tarea11/spot5/E55542961503031J1A02002/SCENE01'
     Spot5DN2TOA(folder)

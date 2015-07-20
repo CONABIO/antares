dc0765f02b9e3fe479a186335be0a616c0c25a35
Erick Palacios <epalacios@conabio.gob.mx>
2015-07-20 20:38:50 +0000

    changes for preprocessing spot
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@239 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/data/raster.py
+++ madmex/mapper/data/raster.py
@@ -1,3 +1,5 @@
+
+
 '''
 Created on 10/06/2015
 
@@ -10,13 +12,20 @@ import logging
 import gdal, gdalconst
 import ogr, osr
 
-from madmex.mapper.base import BaseData, _get_attribute
+from madmex.mapper.base import BaseData, _get_attribute, put_in_dictionary
 
 
 gdal.AllRegister()
 gdal.UseExceptions()
 
 LOGGER = logging.getLogger(__name__)
+
+DRIVER_METADATA = ['driver_metadata'] 
+METADATA_FILE = ['metadata_file']
+PROJECTION = ['properties', 'projection']
+GEO_TRANSFORM = ['properties', 'geotransform']
+DATA_SHAPE = ['properties', 'data_shape']
+FOOTPRINT = ['properties', 'footprint']
 
 class Data(BaseData):
     '''
@@ -30,13 +39,29 @@ class Data(BaseData):
         '''
         super(Data, self).__init__()
         self.image_path = image_path
+        self.metadata ={}
         try:
+            LOGGER.info("Extracting metadata of driver %s" % gdal_format)
+            print "Extracting metadata of driver %s" % gdal_format
             self.driver = gdal.GetDriverByName(gdal_format)
+            self.metadata[DRIVER_METADATA[0]] = self.driver.GetMetadata()
+            #self.driver_metadata = self.driver.GetMetadata()
         except AttributeError:
             LOGGER.error('Cannot access driver for format %s' % gdal_format)
         self.data_file = self._open_file()
-        self.metadata = {}
-        self._extract_metadata()
+        if self.data_file != None:
+            LOGGER.info("Extracting metadata of file %s" % self.data_file)
+            print "Extracting metadata of file %s" % self.data_file
+            self.metadata[METADATA_FILE[0]] = self.data_file.GetMetadata()
+            #self.metadata = self.data_file.GetMetadata()
+            #self.raster_info = {}
+        else:
+            LOGGER.error("Image %s does not provide metadata" % self.data_file)
+        if self.data_file.GetRasterBand(1)!= None:
+            LOGGER.info("Getting properties projection, geotransform, data_shape and footprint of raster %s" % self.data_file)
+            print "Getting properties projection, geotransform, data_shape and footprint of raster %s" % self.data_file
+            self._extract_raster_properties()
+
     def _open_file(self, mode=gdalconst.GA_ReadOnly):
         '''
         Open the raster image file with gdal.
@@ -46,39 +71,58 @@ class Data(BaseData):
             return gdal.Open(self.image_path, mode)
         except RuntimeError:
             LOGGER.error('Unable to open raster file %s', self.image_path)
-    def _extract_metadata(self):
+    def _extract_raster_properties(self):
         '''
-        Extract metadata from the raster image file using gdal functions.
+        Extract some raster info from the raster image file using gdal functions.
         '''
-        self.metadata['projection'] = self.data_file.GetProjection()
-        self.metadata['geotransform'] = self.data_file.GetGeoTransform()
-        self.metadata['data_shape'] = (
-            self.data_file.RasterXSize,
-            self.data_file.RasterYSize,
-            self.data_file.RasterCount
-            )
-        self.metadata['footprint'] = self._get_footprint()
+        self.metadata['properties'] ={'projection': None, 'geotransform': None, 'data_shape': None, 'footprint': None}
+        put_in_dictionary(self.metadata, PROJECTION, self.data_file.GetProjection())
+        put_in_dictionary(self.metadata, GEO_TRANSFORM, self.data_file.GetGeoTransform())
+        put_in_dictionary(self.metadata, DATA_SHAPE, (self.data_file.RasterXSize, self.data_file.RasterYSize, self.data_file.RasterCount))
+        put_in_dictionary(self.metadata, FOOTPRINT, self._get_footprint())
+
     def _get_footprint(self):
         '''
         Returns the extent of the raster image.
         '''
-        ring = ogr.Geometry(ogr.wkbLinearRing)
-        geotransform = self.get_attribute('geotransform')
-        data_shape = self.get_attribute('data_shape')
-        ring.AddPoint_2D(geotransform[0], geotransform[3])
-        ring.AddPoint_2D(geotransform[0] + geotransform[1] * data_shape[0], geotransform[3])
-        ring.AddPoint_2D(
-            geotransform[0] + geotransform[1] * data_shape[0],
-            geotransform[3] + geotransform[5] * data_shape[1]
-            )
-        ring.AddPoint_2D(geotransform[0], geotransform[3] + geotransform[5] * data_shape[1])
-        ring.CloseRings()
-        spacial_reference = osr.SpatialReference()
-        spacial_reference.ImportFromWkt(self.get_attribute('projection'))
-        return self._footprint_helper(ring, spacial_reference)
-    def get_attribute(self, key):
+        try:
+            ring = ogr.Geometry(ogr.wkbLinearRing)
+            geotransform = self.get_attribute(GEO_TRANSFORM)
+            data_shape = self.get_attribute(DATA_SHAPE)
+            ring.AddPoint_2D(geotransform[0], geotransform[3])
+            ring.AddPoint_2D(geotransform[0] + geotransform[1] * data_shape[0], geotransform[3])
+            ring.AddPoint_2D(
+                geotransform[0] + geotransform[1] * data_shape[0],
+                geotransform[3] + geotransform[5] * data_shape[1]
+                )
+            ring.AddPoint_2D(geotransform[0], geotransform[3] + geotransform[5] * data_shape[1])
+            ring.CloseRings()
+            spacial_reference = osr.SpatialReference()
+            spacial_reference.ImportFromWkt(self.get_attribute(PROJECTION))
+            return self._footprint_helper(ring, spacial_reference)
+        except:
+            LOGGER.error('Unable to get footprint of %s', self.image_path)
+            print 'Unable to get footprint'
+    def get_attribute(self, path_to_attribute):
         '''
-        Returns the attribute that is found with the given key in the metadata
-        dictionary.
+        Returns the attribute that is found in the given path
         '''
-        return _get_attribute([key], self.metadata)
+        return _get_attribute(path_to_attribute, self.metadata)
+    def create_from_reference(self, outname, width_raster, height_raster, number_of_bands, type_format, geotransform, projection, options = []):
+        data = self.driver.Create(outname, width_raster, height_raster, number_of_bands, type_format, options)
+        data.SetGeoTransform(geotransform)
+        data.SetProjection(projection)
+        return data
+    def write_raster(self, number_of_bands, data_file, data_to_write):
+        '''
+        data_file: data that will have the data in parameter data_to_write
+        '''
+        for b in range(number_of_bands):
+            data_file.GetRasterBand(b+1).WriteArray(data_to_write[b,:,:])  
+        
+    def close(self):
+        '''
+        Method for closing the gdal file
+        '''
+        self.data_file = None
+
--- madmex/preprocessing/spot5.py
+++ madmex/preprocessing/spot5.py
@@ -4,24 +4,19 @@ Created on 15/07/2015
 @author: erickpalacios
 '''
 from __future__ import unicode_literals
-
-import gdal
-from madmex.preprocessing import base
 from madmex.preprocessing.bundle import spot5 
 
 def Spot5DN2TOA(indir):
     print "Start folder: ", indir
     bundle = spot5.Bundle(indir)
     if bundle.can_identify():
-        #bundle.get_raster()
+        bundle.get_raster_properties()
         bundle.get_sensor()
-        #bundle.calculate_toa()
+        bundle.calculate_toa()
         bundle.export()
     
-    
-
 if __name__ == '__main__':
     
-    folder = '/Volumes/Imagenes_originales/SPOT5/SPOTMarz/SinNubes/E55542961503031J1A02002/SCENE01'
-    #folder = '/Users/erickpalacios/Documents/CONABIO/Tareas/Tarea11/spot5/E55542961503031J1A02002/SCENE01'
+    #folder = '/Volumes/Imagenes_originales/SPOT5/SPOTMarz/SinNubes/E55542961503031J1A02002/SCENE01'
+    folder = '/Users/erickpalacios/Documents/CONABIO/Tareas/Tarea11/spot5/E55542961503031J1A02002/SCENE01'
     Spot5DN2TOA(folder)

d2d54f1e5b2015d84affb68a7d0c516b84391f9e
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-11 22:57:46 +0000

    Cleaned some code formatting.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@266 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/sensor/spot5.py
+++ madmex/mapper/sensor/spot5.py
@@ -3,33 +3,146 @@ Created on 29/06/2015
 
 @author: erickpalacios
 '''
+from __future__ import unicode_literals
+
 from madmex.mapper.base import BaseSensor
 import madmex.mapper.parser.spot5 as spot5
 from datetime import datetime
 
-PROCESSING_LEVEL = ['Dimap_Document', 'Data_Processing', 'PROCESSING_LEVEL']
-SENSOR = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'MISSION']
-PLATFORM = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'MISSION_INDEX']
-CREATION_DATE = ['Dimap_Document', 'Production', 'DATASET_PRODUCTION_DATE']
-ACQUISITION_DATE = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'IMAGING_DATE']
-ACQUISITION_TIME = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'IMAGING_TIME']
-ANGLE = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'VIEWING_ANGLE']
+PROCESSING_LEVEL = [
+    'Dimap_Document',
+    'Data_Processing',
+    'PROCESSING_LEVEL'
+    ]
+SENSOR = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'MISSION'
+    ]
+PLATFORM = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'MISSION_INDEX'
+    ]
+CREATION_DATE = [
+    'Dimap_Document',
+    'Production',
+    'DATASET_PRODUCTION_DATE'
+    ]
+ACQUISITION_DATE = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'IMAGING_DATE'
+    ]
+ACQUISITION_TIME = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'IMAGING_TIME'
+    ]
+ANGLE = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'VIEWING_ANGLE'
+    ]
 CLOUDS = ['clouds']
 QUICKLOOK = ['quicklook']
-BAND_INDEX = ['Dimap_Document', 'Spectral_Band_Info', 'BAND_INDEX']
-BAND_DESCRIPTION = ['Dimap_Document', 'Spectral_Band_Info', 'BAND_DESCRIPTION']
-PHYSICAL_GAIN = ['Dimap_Document', 'Spectral_Band_Info', 'PHYSICAL_GAIN']
-PHYSICAL_BIAS = ['Dimap_Document', 'Spectral_Band_Info', 'PHYSICAL_BIAS']
-TIE_POINT_CRS_X = ['Dimap_Document', 'Geoposition', 'Geoposition_Points', 'Tie_Point', 'TIE_POINT_CRS_X']
-TIE_POINT_CRS_Y = ['Dimap_Document', 'Geoposition', 'Geoposition_Points', 'Tie_Point', 'TIE_POINT_CRS_Y']
-TIE_POINT_DATA_X = ['Dimap_Document', 'Geoposition', 'Geoposition_Points', 'Tie_Point', 'TIE_POINT_DATA_X']
-TIE_POINT_DATA_Y = ['Dimap_Document', 'Geoposition', 'Geoposition_Points', 'Tie_Point', 'TIE_POINT_DATA_Y']
-SCENE_ORIENTATION = ['Dimap_Document', 'Dataset_Frame', 'SCENE_ORIENTATION']
-HORIZONTAL_CS_CODE = ['Dimap_Document', 'Coordinate_Reference_System', 'Horizontal_CS', 'HORIZONTAL_CS_CODE']
-SUN_AZIMUTH = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'SUN_AZIMUTH']
-SUN_ELEVATION = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'SUN_ELEVATION']
-GRID_REFERENCE =['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'GRID_REFERENCE']
-HRG = ['Dimap_Document', 'Dataset_Sources', 'Source_Information', 'Scene_Source', 'SOURCE_DESCRIPTION']
+BAND_INDEX = [
+    'Dimap_Document',
+    'Spectral_Band_Info',
+    'BAND_INDEX'
+    ]
+BAND_DESCRIPTION = [
+    'Dimap_Document',
+    'Spectral_Band_Info',
+    'BAND_DESCRIPTION'
+    ]
+PHYSICAL_GAIN = [
+    'Dimap_Document',
+    'Spectral_Band_Info',
+    'PHYSICAL_GAIN'
+    ]
+PHYSICAL_BIAS = [
+    'Dimap_Document',
+    'Spectral_Band_Info',
+    'PHYSICAL_BIAS'
+    ]
+TIE_POINT_CRS_X = [
+    'Dimap_Document',
+    'Geoposition',
+    'Geoposition_Points',
+    'Tie_Point',
+    'TIE_POINT_CRS_X'
+    ]
+TIE_POINT_CRS_Y = [
+    'Dimap_Document',
+    'Geoposition',
+    'Geoposition_Points',
+    'Tie_Point',
+    'TIE_POINT_CRS_Y'
+    ]
+TIE_POINT_DATA_X = [
+    'Dimap_Document',
+    'Geoposition',
+    'Geoposition_Points',
+    'Tie_Point',
+    'TIE_POINT_DATA_X'
+    ]
+TIE_POINT_DATA_Y = [
+    'Dimap_Document',
+    'Geoposition',
+    'Geoposition_Points',
+    'Tie_Point',
+    'TIE_POINT_DATA_Y'
+    ]
+SCENE_ORIENTATION = [
+    'Dimap_Document',
+    'Dataset_Frame',
+    'SCENE_ORIENTATION'
+    ]
+HORIZONTAL_CS_CODE = [
+    'Dimap_Document',
+    'Coordinate_Reference_System',
+    'Horizontal_CS',
+    'HORIZONTAL_CS_CODE'
+    ]
+SUN_AZIMUTH = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'SUN_AZIMUTH'
+    ]
+SUN_ELEVATION = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'SUN_ELEVATION'
+    ]
+GRID_REFERENCE = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'GRID_REFERENCE'
+    ]
+HRG = [
+    'Dimap_Document',
+    'Dataset_Sources',
+    'Source_Information',
+    'Scene_Source',
+    'SOURCE_DESCRIPTION'
+    ]
 class Sensor(BaseSensor):
     '''
     This class represents a Spot Sensor object. It owns a Spot parser in charge
@@ -67,8 +180,8 @@ class Sensor(BaseSensor):
         self.parser.apply_format(ANGLE, lambda x: float(x))
         self.parser.apply_format(
             ACQUISITION_DATE,
-            lambda x: datetime.strptime(x, "%Y-%m-%d")
+            lambda x: datetime.strptime(x, '%Y-%m-%d')
             )
         self.parser.set_attribute(CLOUDS, 999)
         self.parser.set_attribute(QUICKLOOK, 'PREVIEW.JPG')
-        
+    

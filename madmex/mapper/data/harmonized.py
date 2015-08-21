fe700cf3f039bcafc52bf66c9b08d99fca0d548c
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-21 20:29:29 +0000

    Added an option to harmonize several images instead of just two.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@290 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/data/harmonized.py
+++ madmex/mapper/data/harmonized.py
@@ -3,6 +3,8 @@ Created on 21/08/2015
 
 @author: erickpalacios
 '''
+
+from __future__ import unicode_literals
 from madmex.mapper.base import BaseData, _get_attribute, put_in_dictionary
 import numpy
 import logging
@@ -31,38 +33,55 @@ class Data(BaseData): #TODO: not useful the inheritance from BaseData, change th
         and uniform xrange, yrange, xoffset, yoffset
         '''
         import raster
-        proj_image1 = image1_data_class.get_attribute(raster.PROJECTION)
-        if proj_image1 == image2_data_class.get_attribute(raster.PROJECTION):        
-            geotransform_array = numpy.array([image1_data_class.get_attribute(raster.GEOTRANSFORM), image2_data_class.get_attribute(raster.GEOTRANSFORM)])
-            data_shape_array = numpy.array([image1_data_class.get_attribute(raster.DATA_SHAPE), image2_data_class.get_attribute(raster.DATA_SHAPE)])    
-            self.harmonized_extents ={'projection': None, 'geotransform': None, 'xrange': None, 'yrange': None, 'xoffset': None, 'yoffset': None}
-            put_in_dictionary(self.harmonized_extents, PROJECTION, proj_image1)
-            # Intersect boundary coordinates
-            # Get upper left coordinates
-            ul_x = max(geotransform_array[:, 0])
-            ul_y = min(geotransform_array[:, 3])
-            # Calculate lower right coordinates
-            lr_x = min(geotransform_array[:, 0] + data_shape_array[:, 0] * geotransform_array[:, 1])
-            lr_y = max(geotransform_array[:, 3] + data_shape_array[:, 1] * geotransform_array[:, 5])
-            # Calculate range in x and y dimension in pixels
-            x_range = (lr_x - ul_x) / geotransform_array[0, 1]
-            y_range = (lr_y - ul_y) / geotransform_array[0, 5]
-            # Calculate offset values for each image
-            x_offset = (ul_x - geotransform_array[:, 0]) / geotransform_array[:, 1]
-            y_offset = (ul_y - geotransform_array[:, 3]) / geotransform_array[:, 5]
-            # Calculate unique geo transformation
-            geotransform = (ul_x, geotransform_array[0, 1], 0.0, ul_y, 0.0, geotransform_array[0, 5])
-            put_in_dictionary(self.harmonized_extents, GEOTRANSFORM, geotransform)
-            put_in_dictionary(self.harmonized_extents, XRANGE, x_range)
-            put_in_dictionary(self.harmonized_extents, YRANGE, y_range)
-            put_in_dictionary(self.harmonized_extents, XOFFSET, x_offset)
-            put_in_dictionary(self.harmonized_extents, YOFFSET, y_offset)
-        else:
-            LOGGER.info('Skipping a dataset because of different projection of image %s and image %s' %(image1_data_class.image_path, image2_data_class.image_path))
-
+        self.harmonize_images([image1_data_class, image2_data_class], image1_data_class.get_attribute(raster.PROJECTION))
+    def harmonize_images(self, images, projection):
+        '''
+        Harmonizes a list of images into the minimum common extent. If one of
+        the images is not in the specified projection, it will be ignored.
+        '''
+        import raster
+        geotransforms = []
+        projections = []
+        shapes = []
+        accepted_images = []
+        put_in_dictionary(self.harmonized_extents, PROJECTION, projection)
+        for image in images:
+            if image and image.get_attribute(raster.PROJECTION) == projection:
+                geotransforms.append(image.get_attribute(raster.GEOTRANSFORM))
+                projections.append(image.get_attribute(raster.PROJECTION))
+                shapes.append(image.get_attribute(raster.DATA_SHAPE))
+                accepted_images.append(image)
+            else:
+                LOGGER.warn('Image not in the specified projection, will be ignored.')
+        geotransforms = numpy.array(geotransforms)
+        projections = numpy.array(projections)
+        shapes = numpy.array(shapes)
+        
+        
+        # Intersect boundary coordinates
+        # Get upper left coordinates
+        ul_x = max(geotransforms[:, 0])
+        ul_y = min(geotransforms[:, 3])
+        # Calculate lower right coordinates
+        lr_x = min(geotransforms[:, 0] + shapes[:, 0] * geotransforms[:, 1])
+        lr_y = max(geotransforms[:, 3] + shapes[:, 1] * geotransforms[:, 5])
+        # Calculate range in x and y dimension in pixels
+        x_range = (lr_x - ul_x) / geotransforms[0, 1]
+        y_range = (lr_y - ul_y) / geotransforms[0, 5]
+        # Calculate offset values for each image
+        x_offset = (ul_x - geotransforms[:, 0]) / geotransforms[:, 1]
+        y_offset = (ul_y - geotransforms[:, 3]) / geotransforms[:, 5]
+        # Calculate unique geo transformation
+        geotransform = (ul_x, geotransforms[0, 1], 0.0, ul_y, 0.0, geotransforms[0, 5])
+        put_in_dictionary(self.harmonized_extents, GEOTRANSFORM, geotransform)
+        put_in_dictionary(self.harmonized_extents, XRANGE, x_range)
+        put_in_dictionary(self.harmonized_extents, YRANGE, y_range)
+        put_in_dictionary(self.harmonized_extents, XOFFSET, x_offset)
+        put_in_dictionary(self.harmonized_extents, YOFFSET, y_offset)
+        return self.harmonized_extents
     def get_attribute(self, path_to_attribute):
         '''
         Returns the attribute that is found in the given path.
         '''
         return _get_attribute(path_to_attribute, self.harmonized_extents)                        
-            
+    

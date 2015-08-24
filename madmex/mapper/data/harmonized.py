fb10c7cbde8437264de3bd053a95738a43ef52a6
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-24 22:08:24 +0000

    Added functions to create masks on data array.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@292 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/data/harmonized.py
+++ madmex/mapper/data/harmonized.py
@@ -7,16 +7,45 @@ Created on 21/08/2015
 from __future__ import unicode_literals
 from madmex.mapper.base import BaseData, _get_attribute, put_in_dictionary
 import numpy
+import numpy.ma as ma
 import logging
+from Carbon.TextEdit import WIDTHHook
 
 LOGGER = logging.getLogger(__name__)
 PROJECTION = ['projection']
 GEOTRANSFORM = ['geotransform']
-XRANGE = ['xrange']
-YRANGE = ['yrange']
-XOFFSET = ['xoffset']
-YOFFSET = ['yoffset'] 
+XRANGE = ['x_range']
+YRANGE = ['y_range']
+XOFFSET = ['x_offset']
+YOFFSET = ['y_offset'] 
 
+
+def get_image_mask(x, y, width, height, data, threshold=0):
+    '''
+    This method takes a 2x2 matrix, usually representing an image, and takes a
+    subset of the image using the offsets given by x and y, and the dimensions
+    given by width and height. The remaining argument, is used to create the values
+    of the mask, if the values in the matrix fall under the threshold value, then
+    the mask element in that position will be True; False otherwise.
+    '''
+    if len(data.shape) != 2:
+        raise ValueError('Invalid data dimension, data should be 2 dimensional.')
+    subset = data[x:x + width,y:y + height]
+    mask = numpy.zeros((width, height))
+    numpy.putmask(mask, subset<=threshold, 1)
+    return mask
+
+def get_multiband_image_mask(x, y, width, height, data, threshold=0):
+    '''
+    This method takes as input a multiband image, it flattens the image by applying
+    a minimum over the first axis, obtaining a 2x2 matrix. Then the process of
+    creating a mask is applied to this new matrix.
+    '''
+    if len(data.shape) != 3:
+        raise ValueError('Invalid data dimension, data should be 3 dimensional.')
+    min_data = numpy.min(data, axis=0)
+    return get_image_mask(x, y, width, height, min_data, threshold)
+    
 class Data(BaseData): #TODO: not useful the inheritance from BaseData, change this inheritance to another Base class
     '''
     classdocs
@@ -32,7 +61,6 @@ class Data(BaseData): #TODO: not useful the inheritance from BaseData, change th
         Given two images of class raster create a new image with no data, the same projection, 
         and uniform xrange, yrange, xoffset, yoffset
         '''
-        import raster
         self.harmonize_images([image1_data_class, image2_data_class], image1_data_class.get_attribute(raster.PROJECTION))
     def harmonize_images(self, images, projection):
         '''
@@ -83,5 +111,113 @@ class Data(BaseData): #TODO: not useful the inheritance from BaseData, change th
         '''
         Returns the attribute that is found in the given path.
         '''
-        return _get_attribute(path_to_attribute, self.harmonized_extents)                        
+        return _get_attribute(path_to_attribute, self.harmonized_extents)
+                             
+
+if __name__ == '__main__':
+    import raster
+    image1 = '/LUSTRE/MADMEX/eodata/etm+/23047/2013/2013-06-24/l1t/LE70230472013175ASN00_B1.TIF'
+    image2 = '/LUSTRE/MADMEX/eodata/etm+/23047/2013/2013-01-15/l1g/LE70230472013015ASN00_B1.TIF'
+    image_pair_list = []
+    image_pair_list.append(image1)
+    image_pair_list.append(image2)
+    gdal_format = "GTiff"
+    image1_data_class =raster.Data(image1, gdal_format)
+    image2_data_class = raster.Data(image2, gdal_format)
+    
+    
+    
+    harmonized_class = Data(image1_data_class, image2_data_class)
+    extents = harmonized_class.harmonized_extents
+    print extents
+    
+    x_range = extents['x_range']
+    y_range = extents['y_range']
+    x_offset = extents['x_offset']
+    y_offset = extents['y_offset']
+    
+    
+    print 'x_range : %s' % x_range
+    print 'y_range : %s' % y_range
+    print 'x_offset : %s' % x_offset
+    print 'y_offset : %s' % y_offset
+    
+    image1_data = image1_data_class.read_data_file_as_array()
+    image2_data = image2_data_class.read_data_file_as_array()
+
+    print 'Size of image1 ' , image1_data.shape
+    print 'Size of image2 ' , image2_data.shape
+
+    image1_data_class.close()
+    image2_data_class.close()
+    
+    print numpy.unique(image1_data)
+    
+    
+    first = int(y_offset[0]) 
+    second = int(y_offset[0] + y_range)
+    third = int(x_offset[0])
+    fourth = int(x_offset[0] + x_range)
+    
+    print [first, second]
+    print [third, fourth]
+    
+    a = numpy.min(image1_data[first:second, third:fourth],axis=0)
+    b = numpy.min(image2_data[int(y_offset[1]):int(y_offset[1] + y_range),int(x_offset[1]):int(x_offset[1] + x_range)],axis=0)
+    
+    mask_a = numpy.ones((y_range, x_range))
+    mask_b = numpy.ones((y_range, x_range))
+    
+#   numpy.putmask(mask_a, a<=0, 0)
+#   numpy.putmask(mask_b, b<=0, 0)
+    
+    mask =  numpy.logical_not(numpy.logical_and(mask_a, mask_b))
+    
+    
+
+    print 'a : %s' % a
+    print 'b : %s' % b
+    print 'hello'
+    
+    
+    my_data = numpy.random.rand(10,10)
+    x = 2
+    y = 2
+    width = 3
+    height = 4
+    threshold = .3
+    
+    subset = my_data[x:x+width, y:y+height]
+    
+    x_mask = get_image_mask(x,y,width,height,my_data,threshold)
+    
+    print subset
+    print x_mask
+    
+    masked_data = ma.masked_array(subset, x_mask)
+ 
+    print masked_data
+    
+    print masked_data[~masked_data.mask]
+    
+    
+    multi_data = numpy.random.rand(5, 10, 10)
+    
+    multi_mask = get_multiband_image_mask(x,y,width,height,multi_data,threshold)
+    
+    multi_subset = multi_data[:,x:x+width, y:y+height]
+    
+    
+    #masked_multi_data = ma.masked_array(multi_subset, multi_mask)
+    
+    
+    print multi_subset
+    
+    print multi_mask
+    
+    
+    
+    
+    
+    
     

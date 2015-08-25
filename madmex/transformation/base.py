76755b4c099149e1955de716d593e7a93f810e8f
Erick Palacios <epalacios@conabio.gob.mx>
2015-08-25 18:03:56 +0000

    created folder "transformation", created modules base.py and imad.py in this folder
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@295 df9ad718-c9dd-4117-8c4c-257265001aed

---
+++ madmex/transformation/base.py
@@ -0,0 +1,16 @@
+'''
+Created on 24/08/2015
+
+@author: erickpalacios
+'''
+
+class BaseTransformation(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        print params

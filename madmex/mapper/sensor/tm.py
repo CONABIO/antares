5a05cee490a4a2ff401002c9f411ab370ad64c54
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-26 20:00:36 +0000

    Added sensors for thematic mapper and enhanced thematic mapper.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@312 df9ad718-c9dd-4117-8c4c-257265001aed

---
+++ madmex/mapper/sensor/tm.py
@@ -0,0 +1,18 @@
+'''
+Created on Aug 26, 2015
+
+@author: agutierrez
+'''
+from __future__ import unicode_literals
+
+from madmex.mapper.base import BaseSensor
+
+class Sensor(BaseSensor):
+    '''
+    classdocs
+    '''
+    def __init__(self, metadata_path):
+        '''
+        Constructor
+        '''
+        pass

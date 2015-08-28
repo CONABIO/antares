3c640d7ad785153bdd7843e6d07c41408b007c8e
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-28 17:01:40 +0000

    Refactor on some basic functionality of the imad class, preprocessing, processing, and postprocessing are now called from execute method in parent.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@315 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/transformation/imad.py
+++ madmex/transformation/imad.py
@@ -17,6 +17,8 @@ from madmex.transformation.base import BaseTransformation
 
 LOGGER = logging.getLogger(__name__)
 
+MIN_DELTA = 0.02
+
 class Transformation(BaseTransformation):
     '''
     classdocs
@@ -25,45 +27,56 @@ class Transformation(BaseTransformation):
         '''
         Constructor
         '''
+        self.image1_array = image1_array
+        self.image2_array = image2_array
         self.output = None
-        self.execute(image1_array, image2_array)
-    def preprocessing(self, image1_array, image2_array):
+        self.bands, self.rows, self.columns = image1_array.shape
+        self.image_bands_flattened = None
+        self.index = None
+        self.index_sum = None
+        self.chisqr = None
+        self.MAD = None
+    def preprocessing(self):
         '''
         Function to extract valid pixels and paremeters for imad transformation
         '''
-        bands, rows, cols = image1_array.shape
-        image_bands_flattened = numpy.zeros((2*bands, cols*rows))
-        for k in xrange(bands):
-            image_bands_flattened[k, :] = numpy.ravel(image1_array[k, :, :])
-            image_bands_flattened[bands+k, :] = numpy.ravel(image2_array[k, :, :])
+        image_bands_flattened = numpy.zeros((2 * self.bands, self.columns * self.rows))
+        for k in xrange(self.bands):
+            image_bands_flattened[k, :] = numpy.ravel(self.image1_array[k, :, :])
+            image_bands_flattened[self.bands + k, :] = numpy.ravel(self.image2_array[k, :, :])
         no_data_value = 0 # TODO: every image needs to have nodata value set to 0
         index1_no_data = image_bands_flattened[0, :] == no_data_value
-        index2_no_data = image_bands_flattened[bands, :] == no_data_value
+        index2_no_data = image_bands_flattened[self.bands, :] == no_data_value
         index_no_data = index1_no_data | index2_no_data
-        index = index_no_data == False
-        image_bands_flattened = image_bands_flattened[:, index]
-        index_sum = numpy.sum(index)
-        return (image_bands_flattened, bands, rows, cols, index, index_sum, numpy.ones(int(index_sum)), 1.0, numpy.zeros(bands), 0, (bands+1)*(bands+1), True)
-    def execute(self, image1_array, image2_array, MIN_DELTA = 0.02):
+        self.index = index_no_data == False
+        self.image_bands_flattened = image_bands_flattened[:, self.index]
+        self.index_sum = numpy.sum(self.index)
+        self.wt = numpy.ones(int(self.index_sum))
+        self.delta = 1.0
+        self.iteration = 0
+        self.max_iterations = (self.bands + 1) * (self.bands+1)
+        self.flag = True
+    def processing(self):
         '''
         The imad transformation of two images
         '''
-        image_bands_flattened, bands, rows, cols, index, index_sum, wt, delta, oldrho, iteration, max_iterations, flag = self.preprocessing(image1_array, image2_array)
+
         self.outcorrlist = []
         MIN_DELTA = 0.02
         LOGGER.info('Starting iMAD iterations.')  
-        while (delta > MIN_DELTA) and (iteration < max_iterations) and flag:
+        oldrho = numpy.zeros(self.bands)
+        while (self.delta > MIN_DELTA) and (self.iteration < self.max_iterations) and self.flag:
             try:
-                LOGGER.info("iMAD iteration: %d", iteration)
-                sumw = numpy.sum(wt)
-                means = numpy.average(image_bands_flattened, axis=1, weights=wt)
-                dmc = image_bands_flattened - means[:, numpy.newaxis]
-                dmc = numpy.multiply(dmc, numpy.sqrt(wt))
+                LOGGER.info("iMAD iteration: %d", self.iteration)
+                sumw = numpy.sum(self.wt)
+                means = numpy.average(self.image_bands_flattened, axis=1, weights=self.wt)
+                dmc = self.image_bands_flattened - means[:, numpy.newaxis]
+                dmc = numpy.multiply(dmc, numpy.sqrt(self.wt))
                 sigma = numpy.dot(dmc, dmc.T) / sumw
-                s11 = sigma[0:bands, 0:bands]
-                s22 = sigma[bands:, bands:]
-                s12 = sigma[0:bands, bands:]
-                s21 = sigma[bands:, 0:bands]
+                s11 = sigma[0:self.bands, 0:self.bands]
+                s22 = sigma[self.bands:, self.bands:]
+                s12 = sigma[0:self.bands, self.bands:]
+                s21 = sigma[self.bands:, 0:self.bands]
                 aux1=linalg.solve(s22,s21)
                 lama,a=linalg.eig(numpy.dot(s12,aux1),s11)
                 aux2=linalg.solve(s11,s12)
@@ -76,56 +89,56 @@ class Transformation(BaseTransformation):
                 b = b[:, idx]          
                 #canonical correlations        
                 rho = numpy.sqrt(numpy.real(lamb[idx])) 
-                delta = numpy.sum(numpy.abs(rho - oldrho))
-                if(not math.isnan(delta)):
+                self.delta = numpy.sum(numpy.abs(rho - oldrho))
+                if(not math.isnan(self.delta)):
                     self.outcorrlist.append(rho)
                     #normalize dispersions  
                     tmp1 = numpy.dot(numpy.dot(a.T, s11), a)
                     tmp2 = 1. / numpy.sqrt(numpy.diag(tmp1))
-                    tmp3 = numpy.tile(tmp2, (bands, 1))
+                    tmp3 = numpy.tile(tmp2, (self.bands, 1))
                     a = numpy.multiply(a, tmp3)
                     b = numpy.mat(b)
                     tmp1 = numpy.dot(numpy.dot(b.T, s22), b)
                     tmp2 = 1. / numpy.sqrt(numpy.diag(tmp1))
-                    tmp3 = numpy.tile(tmp2, (bands, 1))
+                    tmp3 = numpy.tile(tmp2, (self.bands, 1))
                     b = numpy.multiply(b, tmp3)
                     #assure positive correlation
                     tmp = numpy.diag(numpy.dot(numpy.dot(a.T, s12), b))
                     b = numpy.dot(b, numpy.diag(tmp / numpy.abs(tmp)))
                     #canonical and MAD variates
-                    U = numpy.dot(a.T , (image_bands_flattened[0:bands, :] - means[0:bands, numpy.newaxis]))    
-                    V = numpy.dot(b.T , (image_bands_flattened[bands:, :] - means[bands:, numpy.newaxis]))          
-                    MAD = U - V  
+                    U = numpy.dot(a.T, (self.image_bands_flattened[0:self.bands, :] - means[0:self.bands, numpy.newaxis]))    
+                    V = numpy.dot(b.T, (self.image_bands_flattened[self.bands:, :] - means[self.bands:, numpy.newaxis]))          
+                    self.MAD = U - V  
                     #new weights        
-                    var_mad = numpy.tile(numpy.mat(2 * (1 - rho)).T, (1, index_sum))    
-                    chisqr = numpy.sum(numpy.multiply(MAD, MAD) / var_mad, 0)
-                    wt = numpy.squeeze(1 - numpy.array(stats.chi2._cdf(chisqr, bands))) 
+                    var_mad = numpy.tile(numpy.mat(2 * (1 - rho)).T, (1, self.index_sum))    
+                    self.chisqr = numpy.sum(numpy.multiply(self.MAD, self.MAD) / var_mad, 0)
+                    self.wt = numpy.squeeze(1 - numpy.array(stats.chi2._cdf(self.chisqr, self.bands))) 
                     oldrho = rho
-                    LOGGER.info('Processing of iteration %d finished [%f] ...', iteration, numpy.max(delta))
-                    iteration += 1
+                    LOGGER.info('Processing of iteration %d finished [%f] ...', self.iteration, numpy.max(self.delta))
+                    self.iteration += 1
                 else:
-                    flag=False
-                    LOGGER.warning("Processing in iteration %d produced error. Taking last MAD of iteration %d", iteration, (iteration - 1))
+                    self.flag=False
+                    LOGGER.warning("Processing in iteration %d produced error. Taking last MAD of iteration %d", self.iteration, (self.iteration - 1))
             except Exception, error:
-                flag = False
-                LOGGER.error("iMAD transform failed with error: %s", str(repr(error))) 
-                LOGGER.error("Processing in iteration %d produced error. Taking last MAD of iteration %d" % (iteration,iteration-1))
-        self.postprocessing(rows, cols, bands, index, chisqr, MAD)
-    def postprocessing(self, rows, cols, bands, index, chisqr, MAD): 
+                self.flag = False
+                LOGGER.error('iMAD transform failed with error: %s', str(repr(error))) 
+                LOGGER.error('Processing in iteration %d produced error. Taking last MAD of iteration %d' % (self.iteration, self.iteration - 1))
+    def postprocessing(self):
         '''
         Reshape to original image size, by including nodata pixels    
         '''
-        MADout = numpy.zeros((int(bands + 1), cols * rows))
-        MADout[0:bands, index] = MAD
-        MADout[bands:(bands + 1), index] = chisqr
+        print 'The number of bands is: %s' % self.bands
+        MADout = numpy.zeros((int(self.bands + 1), self.columns * self.rows))
+        MADout[0:self.bands, self.index] = self.MAD
+        MADout[self.bands:(self.bands + 1), self.index] = self.chisqr
         # close
-        MAD = None
-        chisqr = None
+        self.MAD = None
+        self.chisqr = None
         # return to multidimensional array structure (multispectral image)
         LOGGER.info('Reshaping structure of MAD components')
-        self.output = numpy.zeros((bands + 1, rows, cols))
-        for b in xrange(bands + 1):
-                self.output[b, :, :] = (numpy.resize(MADout[b, :], (rows, cols)))
+        self.output = numpy.zeros((self.bands + 1, self.rows, self.columns))
+        for b in xrange(self.bands + 1):
+                self.output[b, :, :] = (numpy.resize(MADout[b, :], (self.rows, self.columns)))
         # close
         MADout = None
 

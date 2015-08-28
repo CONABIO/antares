65c5e6355a2c1ccab26453f2084b6bc0bd044e6e
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-28 17:43:28 +0000

    Added comments for base class.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@316 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/transformation/base.py
+++ madmex/transformation/base.py
@@ -7,7 +7,9 @@ from __future__ import unicode_literals
 
 class BaseTransformation(object):
     '''
-    classdocs
+    This class is the base for the implementation of an algorithm that tranforms
+    data. Input and output should be standard between implementations so we can
+    reuse code between implementations.
     '''
 
     def __init__(self, params):
@@ -17,13 +19,38 @@ class BaseTransformation(object):
         print params
         
     def execute(self):
+        '''
+        Execute will call inner processes to complete the implementation of this
+        algorithm. The idea behind having three methods instead of one is to have
+        more granular control over the pieces that conform the algorithm.
+        '''
         self.preprocessing()
         self.processing()
         self.postprocessing()
     
     def preprocessing(self):
-        raise NotImplementedError('Subclasses of BaseTransformation must provide a preprocessing() method.')
+        '''
+        This step should prepare the data for the algorithm implemented. It should
+        not depend on external inputs and should not have outputs. The idea is
+        to keep things simple so this class could be overriden for many purposes.
+        '''
+        raise NotImplementedError(
+            'Subclasses of BaseTransformation must provide a preprocessing() method.'
+            )
     def processing(self):
-        raise NotImplementedError('Subclasses of BaseTransformation must provide a processing() method.')
+        '''
+        The implementation of the transformation algorithm should go inside here
+        it transforms the input variables into something useful that can later be
+        plugged in another process.
+        '''
+        raise NotImplementedError(
+            'Subclasses of BaseTransformation must provide a processing() method.'
+            )
     def postprocessing(self):
-        raise NotImplementedError('Subclasses of BaseTransformation must provide a postprocessing() method.')
+        '''
+        Once the algorithm has finished, if some extra process is needed in order
+        to have a final product, those steps should go inside here.
+        '''
+        raise NotImplementedError(
+            'Subclasses of BaseTransformation must provide a postprocessing() method.'
+            )

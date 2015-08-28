2fc0a6c22ab9bdb5cb26c8f37a22803878db6658
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-28 17:01:40 +0000

    Refactor on some basic functionality of the imad class, preprocessing, processing, and postprocessing are now called from execute method in parent.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@315 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/transformation/base.py
+++ madmex/transformation/base.py
@@ -3,6 +3,7 @@ Created on 24/08/2015
 
 @author: erickpalacios
 '''
+from __future__ import unicode_literals
 
 class BaseTransformation(object):
     '''
@@ -14,3 +15,15 @@ class BaseTransformation(object):
         Constructor
         '''
         print params
+        
+    def execute(self):
+        self.preprocessing()
+        self.processing()
+        self.postprocessing()
+    
+    def preprocessing(self):
+        raise NotImplementedError('Subclasses of BaseTransformation must provide a preprocessing() method.')
+    def processing(self):
+        raise NotImplementedError('Subclasses of BaseTransformation must provide a processing() method.')
+    def postprocessing(self):
+        raise NotImplementedError('Subclasses of BaseTransformation must provide a postprocessing() method.')

04bc6b25f9674d358eab1202edd886e6824adfee
Amaury Gutiérrez <agutierrez@conabio.gob.mx>
2015-08-31 14:15:50 +0000

    Landsat bundle classes.
    git-svn-id: http://madmexservices.conabio.gob.mx:8082/svn/madmex_antares/trunk@318 df9ad718-c9dd-4117-8c4c-257265001aed

--- madmex/mapper/bundle/landsat7.py
+++ madmex/mapper/bundle/landsat7.py
@@ -5,44 +5,40 @@ Created on Jul 22, 2015
 '''
 from __future__ import unicode_literals
 
-from madmex.configuration import SETTINGS
-from madmex.mapper.base import BaseBundle
-from madmex.mapper.bundle._landsat import get_landsat_files
+from madmex.mapper.bundle._landsat import LandsatBaseBundle
+from madmex.mapper.sensor import etmplus
 
 
+FORMAT = 'GTiff'
 _MISSION = '7'
+_NAME = 'Landsat 7'
 
-class Bundle(BaseBundle):
+class Bundle(LandsatBaseBundle):
     '''
     A class to create a memory representation of a Landsat 7 image including its
     metadata files. It is also responsible of creating a database object to be
     persisted.
     '''
-
-
     def __init__(self, path):
         '''
         Constructor
         '''
-        self.path = path
-        self.file_dictionary = get_landsat_files(_MISSION)
-        self._look_for_files()
-        self.output_directory = None
+        super(Bundle, self).__init__(path)
+    def get_format_file(self):
+        return FORMAT
+    def get_sensor_module(self):
+        return etmplus  
+    def get_mission(self):
+        '''
+        Returns the mission for this particular implementation of the landsat
+        satellite.
+        '''
+        return _MISSION
     def get_name(self):
         '''
         Returns the name of the bundle.
         '''
-        return 'Landsat 7'
-    def get_output_directory(self):
-        '''
-        Creates the output directory where the files in this bundle will be
-        persisted on the file system.
-        '''
-        if self.output_directory is None:
-            destination = getattr(SETTINGS, 'TEST_FOLDER')
-            self.output_directory = destination
-        return self.output_directory
-
+        return _NAME
 if __name__ == '__main__':
     path = '/LUSTRE/MADMEX/eodata/etm+/36041/2005/2005-12-22/l1t'
     bundle = Bundle(path)
--- madmex/mapper/bundle/landsat8.py
+++ madmex/mapper/bundle/landsat8.py
@@ -5,36 +5,37 @@ Created on Aug 26, 2015
 '''
 from __future__ import unicode_literals
 
-from madmex.configuration import SETTINGS
-from madmex.mapper.base import BaseBundle
+from madmex.mapper.bundle._landsat import LandsatBaseBundle
+from madmex.mapper.sensor import etmplus
 
-class Bundle(BaseBundle):
+
+FORMAT = 'GTiff'
+_MISSION = '8'
+_NAME = 'Landsat 8'
+
+class Bundle(LandsatBaseBundle):
     '''
-    A class to create a memory representation of a Landsat image including its
+    A class to create a memory representation of a Landsat 5 image including its
     metadata files. It is also responsible of creating a database object to be
     persisted.
     '''
-
     def __init__(self, path):
         '''
         Constructor
         '''
-        self.path = path
-        self.file_dictionary = {
-                                
-                                }
-        self.output_directory = None
+        super(Bundle, self).__init__(path)
+    def get_format_file(self):
+        return FORMAT
+    def get_sensor_module(self):
+        return etmplus
+    def get_mission(self):
+        '''
+        Returns the mission for this particular implementation of the landsat
+        satellite.
+        '''
+        return _MISSION
     def get_name(self):
         '''
         Returns the name of the bundle.
         '''
-        return 'Landsat 8'
-    def get_output_directory(self):
-        '''
-        Creates the output directory where the files in this bundle will be
-        persisted on the file system.
-        '''
-        if self.output_directory is None:
-            destination = getattr(SETTINGS, 'TEST_FOLDER')
-            self.output_directory = destination
-        return self.output_directory
+        return _NAME

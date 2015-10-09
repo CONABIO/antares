6f691e1b235c8465fe10b84ba42fd16e76e42cda
palmoreck <palmoreck@172.16.13.183>
2015-10-09 15:24:41 -0500

    Just added comments to functions in maskingwithreference.py

--- madmex/preprocessing/maskingwithreference.py
+++ madmex/preprocessing/maskingwithreference.py
@@ -18,6 +18,9 @@ GENERALIZE = False
 MORPHING_SIZE = 10
 #TODO: this module is lacking of landmasking....see rapideyeclouds.py of madmex_old
 def masking(re_directory, re_sensor_metadata, re_raster_array, re_raster_metadata):
+    '''
+    General function for controlling masking
+    '''
     tile_id = re_sensor_metadata(rapideye.TILE_ID)
     re_directory = '/Users/erickpalacios/Documents/CONABIO/Tareas/4_RedisenioMadmex/2_Preprocesamiento/Rapideye/l3a'
     re_directory = '/Users/erickpalacios/Documents/CONABIO/Tareas/4_RedisenioMadmex/2_Preprocesamiento/Rapideye/CloudMasking/RE_1649125/1649125_2014-01-23_RE4_3A_301519'
@@ -27,6 +30,9 @@ def masking(re_directory, re_sensor_metadata, re_raster_array, re_raster_metadat
     image_mask_path = mask_clouds_and_shadows(re_raster_metadata, re_raster_array, image_array_difference, data_shape_reference, solar_zenith, solar_azimuth, re_directory, geotransform_reference, projection_reference)
     return image_mask_path
 def reference_for_tile(re_raster_metadata, folder, tile_id, image_array):
+    '''
+    Get reference image for given tile id
+    '''
     cloud_cover = 100
     sensor_id = 1
     product_id = 2
@@ -37,13 +43,16 @@ def reference_for_tile(re_raster_metadata, folder, tile_id, image_array):
     print("Number of acquired images: %d" % len(images_references_paths))
     return get_image_array_difference(re_raster_metadata, folder, images_references_paths, image_array, len(images_references_paths))
 def get_image_array_difference(re_raster_metadata, folder, images_references_paths, image_array, max_number_of_images):
+    '''
+    Return array of differences between reference image (stacking of bands) and image_array
+    '''
     data_shape, geotransform, projection, list_with_arrays_stack_per_band = get_stack_arrays_per_band(images_references_paths, max_number_of_images)
     band_medians = numpy.zeros([data_shape[2], data_shape[1], data_shape[0]])    
     number_of_bands = data_shape[2]
     for k in range(number_of_bands-1, -1, -1):
         band_medians[k, :, :] = numpy.median(list_with_arrays_stack_per_band[k], axis = 0)
         list_with_arrays_stack_per_band.pop()
-    image_reference_path = folder + '/re_reference_new_stacking.tif'
+    image_reference_path = folder + '/re_reference.tif'
     options_to_create = default_options_for_create_raster_from_reference(re_raster_metadata)
     create_raster_tiff_from_reference(re_raster_metadata, options_to_create, image_reference_path, band_medians)
     LOGGER.info("RE reference image: %s" % image_reference_path)
@@ -56,8 +65,10 @@ def get_image_array_difference(re_raster_metadata, folder, images_references_pat
     return data_shape, geotransform, projection, image_array_difference
 def get_stack_arrays_per_band(images_references_paths, max_number_of_images):
     '''
-    Check consistency of folders and get stack arrays for every band
+    Check consistency of folders and get stack arrays for every band. Return a list with k number 
+    of bands. Every element of the list is a stack array
     '''
+    #TODO: change this line to identify different sensors
     from madmex.mapper.bundle.rapideye import Bundle
     number_of_identified_images = 0
     list_with_arrays_stack_per_band = list()
@@ -85,6 +96,9 @@ def get_stack_arrays_per_band(images_references_paths, max_number_of_images):
             list_with_arrays_stack_per_band[k] = list_with_arrays_stack_per_band[k][0:number_of_identified_images, :, :]
     return (data_shape, geotransform, projection, list_with_arrays_stack_per_band)
 def mask_clouds_and_shadows(re_raster_metadata, image_array, image_array_difference, data_shape_reference, solar_zenith, solar_azimuth, folder, geotransform, projection):
+    '''
+    This use function calculate_cloud_shadow in preprocessing.masking.py
+    '''
     clouds =  filter_median((numpy.sum(image_array_difference, axis=0) > 30000).astype(numpy.int), 13)
     shadows = filter_median((numpy.sum(image_array_difference[3:, :, :], axis=0) < -5500).astype(numpy.int), 13)
     if GENERALIZE:

f464035b104720e1a1d806fb145998823b1dec71
amaurs <amaury.gtz@gmail.com>
2016-01-19 12:04:17 -0600

    Testing new repository, now living in github.

---
+++ madmex/core/controller/commands/statistics.py
@@ -0,0 +1,20 @@
+'''
+Created on Jan 19, 2016
+
+@author: agutierrez
+'''
+
+from __future__ import unicode_literals
+
+from madmex.core.controller.base import BaseCommand
+
+
+class Command(BaseCommand):
+    '''
+    classdocs
+    '''
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, **options):
+        pass

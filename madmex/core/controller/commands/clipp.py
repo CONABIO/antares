fce06c8e2452bfa706d3da0583073b551a6b92bb
amaurs <amaury.gtz@gmail.com>
2016-03-14 15:55:55 -0600

    Added command to clipp rasters using a shape file.

---
+++ madmex/core/controller/commands/clipp.py
@@ -0,0 +1,36 @@
+'''
+Created on Mar 14, 2016
+
+@author: agutierrez
+'''
+from __future__ import unicode_literals
+
+from madmex.core.controller.base import BaseCommand
+
+
+class Command(BaseCommand):
+    '''
+    classdocs
+    '''
+    def add_arguments(self, parser):
+        '''
+        Adds the sum argument for this command, of course this will change in
+        the final implementation.
+        '''
+        parser.add_argument('--shape', nargs='*', help='This is a stub for the, \
+            change detection command, right now all it does is sum numbers in \
+            the list.')
+        parser.add_argument('--raster', nargs='*', help='This is a stub for the, \
+            change detection command, right now all it does is sum numbers in \
+            the list.')
+    def handle(self, **options):
+        '''
+        In this example command, the values that come from the user input are
+        added up and the result is printed in the screen.
+        '''
+        shape = options['shape']
+        raster = options['raster']
+        
+        print shape
+        print raster
+

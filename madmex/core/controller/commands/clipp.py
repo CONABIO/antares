d5845da4a10ccf44a548c4f5ca04928b3c62556a
amaurs <amaury.gtz@gmail.com>
2016-03-16 18:03:39 -0600

    Added function to split a shape into its polygon components.

--- madmex/core/controller/commands/clipp.py
+++ madmex/core/controller/commands/clipp.py
@@ -8,10 +8,60 @@ from __future__ import unicode_literals
 import os
 
 from osgeo import ogr
+import osr
 
 from madmex.core.controller.base import BaseCommand
+from madmex.util import create_filename_from_string, create_file_name
 
 
+def split_shape_into_features(shape_name, destination_directory, column):
+    driver = ogr.GetDriverByName(str('ESRI Shapefile'))
+    shape = driver.Open(shape_name, 0)
+    layer = shape.GetLayer()
+    layer_name = layer.GetName()
+    spatial_reference = layer.GetSpatialRef()
+    
+
+    
+    in_feature = layer.GetNextFeature()
+    
+    while in_feature:
+        
+        
+        encoding = 'utf-8'
+        in_feature_name = create_filename_from_string(in_feature.GetField(column).decode(encoding))
+        output_name = create_file_name(destination_directory, in_feature_name + '.shp')
+        
+        if os.path.exists(output_name):
+            driver.DeleteDataSource(output_name)
+        datasource = driver.CreateDataSource(output_name)
+    
+        outLayer = datasource.CreateLayer(layer_name, spatial_reference, geom_type=ogr.wkbPolygon)
+    
+        inLayerDefn = layer.GetLayerDefn()
+        for i in range(0, inLayerDefn.GetFieldCount()):
+            fieldDefn = inLayerDefn.GetFieldDefn(i)
+            outLayer.CreateField(fieldDefn)
+        
+        outLayerDefn = outLayer.GetLayerDefn()
+        
+        
+        
+        geometry = in_feature.GetGeometryRef()
+        
+        out_feature = ogr.Feature(outLayerDefn)
+        out_feature.SetGeometry(geometry)
+        
+        for i in range(0, outLayerDefn.GetFieldCount()):
+            out_feature.SetField(outLayerDefn.GetFieldDefn(i).GetNameRef(), in_feature.GetField(i))
+            
+        outLayer.CreateFeature(out_feature)
+        out_feature.Destroy()
+        in_feature.Destroy()
+        in_feature = layer.GetNextFeature()
+    shape.Destroy()
+    datasource.Destroy()
+    
 class Command(BaseCommand):
     '''
     classdocs
@@ -38,11 +88,5 @@ class Command(BaseCommand):
         print os.path.exists(shape_name)
         print raster
 
-        driver = ogr.GetDriverByName(str('ESRI Shapefile'))
-        shape = driver.Open(shape_name, 0)
-
-        layer = shape.GetLayer()
-
-        for feature in layer:
-            print dir(feature)
-            print feature.GetField(2)
+        split_shape_into_features(shape_name,'/Users/amaury/Documents/anp/splited/', 2)
+            

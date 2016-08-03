bf3885a25e97a78a9558d894d7a6daadb5dc3b26
palmoreck <palmoreck@gmail.com>
2016-08-03 12:13:32 -0500

    added bundle for landsat8

--- madmex/mapper/bundle/landsat8.py
+++ madmex/mapper/bundle/landsat8.py
@@ -12,6 +12,7 @@ from madmex.mapper.sensor import etmplus
 FORMAT = 'GTiff'
 _MISSION = '8'
 _NAME = 'Landsat 8'
+_LETTER = 'C'
 
 class Bundle(LandsatBaseBundle):
     '''
@@ -34,8 +35,21 @@ class Bundle(LandsatBaseBundle):
         satellite.
         '''
         return _MISSION
+    def get_letter(self):
+        '''
+        Files after 2012 have a letter to distinguish the different sensors
+        In the case of landsat8, the LEDAPS process only can be applied to files with the letter C,
+        that is, only for the oli tirs sensor
+        '''
+        return _LETTER
     def get_name(self):
         '''
         Returns the name of the bundle.
         '''
         return _NAME
+
+if __name__ == '__main__':
+    path = '/LUSTRE/MADMEX/eodata/oli_tirs/21048/2013/2013-04-15/l1t/'
+    bundle = Bundle(path)
+    print bundle.get_files()
+    print bundle.can_identify()

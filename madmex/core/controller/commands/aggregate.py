bdc4130dc1658ba5c195eca1f72da623ffd6c68b
amaurs <amaury.gtz@gmail.com>
2016-08-04 08:35:03 -0500

    Added details to aggregate class.

--- madmex/core/controller/commands/aggregate.py
+++ madmex/core/controller/commands/aggregate.py
@@ -11,11 +11,23 @@ import sys
 
 import gdal
 import gdalconst
+import numpy
 
 from madmex.core.controller.base import BaseCommand
 from madmex.core.controller.commands.indexes import open_handle
-from madmex.mapper.data._gdal import create_empty_raster_from_reference
+from madmex.mapper.data._gdal import create_empty_raster_from_reference, \
+    create_raster_from_reference
 
+
+INITIAL_ARRAY = [[1,2,3,8,9,10,11,12,13,16],
+               [4,5,6,7,17,18,19,21,22,23],
+               [14,15,20,24,25,26],
+               [27],
+               [28],
+               [29],
+               [30],
+               [31]]
+FINAL_ARRAY = [1,2,3,4,5,6,7,8]
 
 class Command(BaseCommand):
     '''
@@ -29,12 +41,14 @@ class Command(BaseCommand):
         '''
         parser.add_argument('--path', nargs='*')
         parser.add_argument('--output', nargs='*')
-    def handle(self, **options):
-        print 'hello'
+
+    def method_one(self, options):
         path = options['path'][0]
         output = options['output'][0]
         
         print path
+        print INITIAL_ARRAY
+        print FINAL_ARRAY
         #data_array = open_handle(path)
         
         outDataset = create_empty_raster_from_reference(output, path)
@@ -65,9 +79,40 @@ class Command(BaseCommand):
                 outputLine = str(outputLine) + struct.pack('f', new_pixel)
             outDataset.GetRasterBand(1).WriteRaster(0, row, classification_band.XSize, 1, outputLine, buf_xsize=classification_band.XSize, buf_ysize=1, buf_type=gdal.GDT_Float32)
             del outputLine
-            
-            if (row%100) == 0:
-                print row
+                
+    def method_two(self, options):
+        path = options['path'][0]
+        output = options['output'][0]
         
+        print path
+        
+        data_array = open_handle(path)
+        original_shape = data_array.shape
+        new_array = numpy.ravel(data_array)
+        
+        for i in xrange(len(INITIAL_ARRAY)):
+            # list to numpy array
+            nparray = numpy.array(INITIAL_ARRAY[i])
+            print(nparray)
+            found_idx = numpy.in1d(data_array,nparray)
+            print(FINAL_ARRAY[i])
+            new_array[found_idx] = FINAL_ARRAY[i]
+        print output
+        create_raster_from_reference(output, new_array.reshape(original_shape), path)
+        
+        
+    
+    
+    def handle(self, **options):
+        print 'hello'
+        import time
+        #start_time = time.time()
+        #self.method_one(options)
+        #print("--- %s seconds ---" % (time.time() - start_time))
+        
+        
+        start_time = time.time()
+        self.method_two(options)
+        print("--- %s seconds ---" % (time.time() - start_time))
         print 'Dataset was written.'
         

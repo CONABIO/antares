2ba8f00d6763c6a8d69c42342291638da711f3d4
amaurs <amaury.gtz@gmail.com>
2016-09-27 11:20:29 -0500

    Added code to calculate the area of a certain type in the maps.

--- madmex/core/controller/commands/statistics.py
+++ madmex/core/controller/commands/statistics.py
@@ -6,20 +6,27 @@ Created on Jan 19, 2016
 
 from __future__ import unicode_literals
 
+import json
 import logging
 import math
 
 import gdal
 import numpy
 from numpy.f2py.rules import aux_rules
+from scipy.constants.constants import hectare
 
 from madmex.core.controller.base import BaseCommand
 from madmex.core.controller.commands import get_bundle_from_path
-from madmex.util import get_base_name
+from madmex.core.controller.commands.aggregate import INITIAL_ARRAY
+from madmex.util import get_base_name, get_parent, create_file_name
 
 
 LOGGER = logging.getLogger(__name__)
 BUNDLE_PACKAGE = 'madmex.mapper.bundle'
+
+INITIAL_ARRAY = {'Bosque' : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,123,124,200,210],
+                 'No Bosque' : [27,28,30,31],
+                 'Otros' : [29,98,99]}
 
 def _get_bundle_from_path(path):
     '''
@@ -40,6 +47,7 @@ class Command(BaseCommand):
         be ingested.
         '''
         parser.add_argument('--path', nargs='*')
+        parser.add_argument('--target', nargs='*')
 
     def handle(self, **options):
         '''
@@ -47,23 +55,40 @@ class Command(BaseCommand):
         '''
         interest_band = 1
         
+        
+        
         for image_path in options['path']:
+            
+                print image_path
+                
+                
                 ds = gdal.Open(image_path)
-                bands = ds.RasterCount                
+                bands = ds.RasterCount           
+                
+                geotransform = ds.GetGeoTransform()     
+                
+                x_resolution = geotransform[1]
+                y_resolution = geotransform[5]
+                
+                pixel_area = abs(x_resolution * y_resolution)
+
                 
                 array = numpy.array(ds.GetRasterBand(interest_band).ReadAsArray())
                 
                 flat = numpy.ravel(array)
                 
                 length = len(flat)
+                parent = get_parent(image_path)
+                basename = '%s.txt' % get_base_name(image_path)
                 
-                print get_base_name(image_path)
+                target = create_file_name(parent, basename)
+                
+        
+                
                 count = 1                
                 values = {}
-                print length
 
                 progress = 0
-                print progress
                 for value in flat:
                     count = count + 1
                     if not values.get(value):
@@ -78,7 +103,17 @@ class Command(BaseCommand):
                         if not aux == progress:
                             print  str(int(progress)) + '%\r'
                 self.print_dict(values)
+                
     
+        
+                added = self.add_up(INITIAL_ARRAY, values)
+                area = self.transform_to_area(added, pixel_area)
+        
+        
+                with open(target, "a") as f:
+                    json.dump(area, f)
+        
+        
     def print_dict(self, values):
         for key in values.keys():
             if not key == 0:
@@ -97,4 +132,23 @@ class Command(BaseCommand):
             if not key == 0:
                 print values.get(key) * 100 / float(total)
         
+    def add_up(self, classifications, values):
+        result = {}
+        for classfification in classifications.keys():
+            counter = 0
+            for key in values.keys():
+                if key in classifications.get(classfification):
+                    counter = counter + values.get(key)
+                    
+            result[classfification] = counter
+        return result
         
+    def transform_to_area(self, values, area_in_pixel):
+        result = {}
+        for key in values.keys():
+            result[key] = values.get(key) * area_in_pixel / hectare
+        return result
+        
+        
+        
+            

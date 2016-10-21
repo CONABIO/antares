f4ad7d807d6ca0fb29c29f3297359d7b0e7a9904
robmartz <rmartinez@conabio.gob.mx>
2016-10-21 17:32:26 -0500

    reading pixel's resolution on rasters inputs

--- madmex/core/controller/commands/tth.py
+++ madmex/core/controller/commands/tth.py
@@ -29,17 +29,33 @@ class Command(BaseCommand):
         '''
         
         '''
-        #parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-        parser.add_argument('tth', nargs='*', )
-        parser.add_argument('--pathList', nargs=1, help='Raster file directory')
+        parser.add_argument('tth', nargs='*' )
         
 
     def handle(self, **options):
         '''
         
         '''
+        raster_path1 = options['tth'][0]
+        raster_path2 = options['tth'][1]
+        LOGGER.info('Reading raster path initial year: %s'  % (raster_path1) )
+        area_raster1, info_at_open1 = self.get_pixel_info(raster_path1)
+        LOGGER.info('Pixel area in raster %s: %s'  % (os.path.basename(raster_path1), str(area_raster1)) )
         
-        LOGGER.info('First line on log file')
-        with open("file.log","a+") as f:
-            f.write('Line')
-            
+        LOGGER.info('Reading raster path final year:   %s'  % (raster_path2) )
+        area_raster2, info_at_open2 = self.get_pixel_info(raster_path2)
+        LOGGER.info('Pixel area in raster %s: %s'  % (os.path.basename(raster_path2), str(area_raster2)) )
+
+        
+    def get_pixel_info(self, path):
+        '''
+        
+        '''
+        ds = gdal.Open(path)
+        bands = ds.RasterCount
+        geotransform = ds.GetGeoTransform()
+        x_resolution = geotransform[1]
+        y_resolution = geotransform[5]
+        pixel_area   = abs(x_resolution * y_resolution)
+        
+        return pixel_area, ds

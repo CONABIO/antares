f000e745998039f4f1e6ac710923612eb602d36c
robmartz <rmartinez@conabio.gob.mx>
2016-11-03 17:43:57 -0600

    filtering classes in both rasters to tth analysis

--- madmex/core/controller/commands/tth.py
+++ madmex/core/controller/commands/tth.py
@@ -8,13 +8,14 @@ from __future__ import unicode_literals
 from decimal import Decimal
 from madmex.core.controller.base import BaseCommand
 from scipy.constants.constants import hectare
-
-import sys
+from operator import itemgetter
 import os
 import gdal
 import numpy as np
-import re
 import logging
+
+
+#from test.pystone import nargs
 
 
 
@@ -29,20 +30,51 @@ class Command(BaseCommand):
         '''
         
         '''
-        parser.add_argument('tth', nargs='*' )
-        
+        parser.add_argument('tth', nargs='*', help='Some general help' )
+        parser.add_argument('--rasterPathIni', nargs='*', help='Some help on raster path initial')
+        parser.add_argument('--rasterPathFin', nargs='*', help='Some help on raster path initial')
+        parser.add_argument('--yearIni', nargs=1, help='Some help on initial year')
+        parser.add_argument('--yearFin', nargs=1, help='Some help on final year')
+    
+
 
     def handle(self, **options):
         '''
         
         '''
+        rasterIni = options['rasterPathIni'][0]
+        rasterFin = options['rasterPathFin'][0]
+        yearIni = options['yearIni'][0]
+        yearFin = options['yearFin'][0]
+        
+        
+        pixel_area, info_at_open= self.pixel_info(rasterIni)
+        LOGGER.info('Pixel area in raster %s: %s [m2]'  % ( os.path.basename(rasterIni), str(pixel_area) ))
+        arr_class_id_ini, area_arr_ini = self.class_info(info_at_open, rasterIni, pixel_area)
                 
-        for j in range(len(options['tth'])):
-            pixel_area, info_at_open= self.pixel_info(options['tth'][j])
-            LOGGER.info('Pixel area in raster %s: %s [m2]'  % ( os.path.basename(options['tth'][j]), str(pixel_area) ))
-            self.class_info(info_at_open, options['tth'][j], pixel_area)
+        pixel_area, info_at_open= self.pixel_info(rasterFin)
+        LOGGER.info('Pixel area in raster %s: %s [m2]'  % ( os.path.basename(rasterFin), str(pixel_area) ))
+        arr_class_id_fin, area_arr_fin = self.class_info(info_at_open, rasterFin, pixel_area)
+
+        matching_class = list(set(arr_class_id_ini).intersection(arr_class_id_fin))
+        orphan_class = list(set(list(arr_class_id_ini)).symmetric_difference(list(arr_class_id_fin)))
+        
+        print matching_class
+        print orphan_class
+        print list(arr_class_id_ini)
+        print list(arr_class_id_fin)
+        
+        # computes the "tth" anaysis only for those classes in both rasters
+         
+        for i in range(len(matching_class)):
+            if matching_class[i] in arr_class_id_ini and matching_class[i] in arr_class_id_fin :
+                index_arr_class_ini = list(arr_class_id_ini).index(matching_class[i])
+                index_arr_class_fin = list(arr_class_id_fin).index(matching_class[i])
+                print matching_class[i], area_arr_ini[index_arr_class_ini], area_arr_fin[index_arr_class_fin]
             
-            
+                    
+                    
+    
     def pixel_info(self, path):
         '''
         
@@ -66,21 +98,24 @@ class Command(BaseCommand):
         raster_array   = np.array(raster.GetRasterBand(1).ReadAsArray())
         arr_class_info = np.unique(raster_array, return_counts=True)
         LOGGER.info('Getting classification info from %s', os.path.basename(raster_name))
-
+        area_arr = []
         for i in range(len(arr_class_info[0])):
             class_id = arr_class_info[0][i]
             num_pixels_per_class = arr_class_info[1][i]
-            area_per_class = self.compute_area(num_pixels_per_class, pixel_area)
+            area_per_class = self.area(num_pixels_per_class, pixel_area)
+            area_arr.append(area_per_class)
             print 'Class ID: ', class_id, '\t', 'No. Pixels per class: ', num_pixels_per_class, '\t', 'Area per class [ha]:', area_per_class
-            
+        
+        return arr_class_info[0], area_arr
                     
-    def compute_area(self, class_pixels, pixel_area):
+                    
+    def area(self, class_pixels, pixel_area):
         '''
         '''
         area_class = (pixel_area * class_pixels) / hectare
         return area_class
+    
                 
-            
                 
                 
                 

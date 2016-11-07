182449807709a25d3de94bd05d815b15e65e9f2f
palmoreck <palmoreck@gmail.com>
2016-11-07 16:08:40 -0600

    adding functionality for rasterize function

--- madmex/processing/vector.py
+++ madmex/processing/vector.py
@@ -4,12 +4,19 @@ Created on 30/08/2016
 @author: erickpalacios
 '''
 import gdal
-def rasterize_vector(target_ds,source_layer, band_list, burn_values):
+def rasterize_vector(target_ds,source_layer, band_list, burn_values,options=None):
     '''
     Good reference for rasterizing a vector for an specific attribute:
     http://gis.stackexchange.com/questions/53417/programmatic-raster-vector-calculation?rq=1 
     '''
-    if gdal.RasterizeLayer(target_ds, band_list, source_layer, burn_values=burn_values)  != 0:
-        raise Exception("error rasterizing layer: %s" % source_layer)
-    else:
-        return 0
+    if burn_values:
+        if gdal.RasterizeLayer(target_ds, band_list, source_layer, burn_values=burn_values)  != 0:
+            raise Exception("error rasterizing layer: %s" % source_layer)
+        else:
+            return 0
+    elif options:
+        if gdal.RasterizeLayer(target_ds, band_list, source_layer, options = options)  != 0:
+            raise Exception("error rasterizing layer: %s" % source_layer)
+        else:
+            return 0
+        

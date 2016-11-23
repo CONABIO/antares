726cd4cfb00bbfe83ff8c221df28f74477eaf016
palmoreck <palmoreck@gmail.com>
2016-11-23 11:29:47 -0600

    debugging

--- madmex/mapper/data/dataframe.py
+++ madmex/mapper/data/dataframe.py
@@ -78,6 +78,8 @@ def outlier_elimination_for_dataframe(dataframe, column_name_of_ids,  column_nam
     
     for i in range(len(list_of_classes)):
         df = dataframe_subset.loc[list_of_classes[i]]
+        if list_of_classes == 90.0:
+            print df
         object_ids_per_class = object_ids_new_indexing.loc[list_of_classes[i]][column_name_of_ids]
         LOGGER.info('Number of objects: %s in class %s' % (len(df.index), list_of_classes[i]))
         LOGGER.info('Just checking : number of objects: %s in class %s of object_ids_per_class dataframe' %(len(object_ids_per_class.index), list_of_classes[i]))

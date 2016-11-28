e4323f353e6afd19475b0778017ce46a470e9f3c
amaurs <amaury.gtz@gmail.com>
2016-11-28 16:58:36 -0600

    Command used to preprocess Matt Hansen maps.

---
+++ madmex/core/controller/commands/matthansen.py
@@ -0,0 +1,65 @@
+'''
+Created on Nov 28, 2016
+
+@author: agutierrez
+'''
+from __future__ import unicode_literals
+
+from zipfile import ZipFile
+
+import gdal
+import numpy
+from scipy.fftpack import ifftn
+
+from madmex import _
+from madmex.core.controller.base import BaseCommand
+from madmex.core.controller.commands.indexes import open_handle
+from madmex.mapper.data._gdal import create_raster_from_reference
+from madmex.remote.dispatcher import LocalProcessLauncher
+from madmex.util import get_contents_from_folder, create_file_name, \
+    get_base_name, create_directory_path, remove_file, remove_directory
+
+
+def tile_map(image_path, output, x_tile_size, y_tile_size, x_offset, y_offset):
+    gdaltranString = ['gdal_translate', '-of', 'GTIFF', '-srcwin', str(x_offset),  str(y_offset) , str(x_tile_size) ,  str(y_tile_size) , image_path, output]
+    shell_string = ' '.join(gdaltranString)    
+    launcher = LocalProcessLauncher()
+    launcher.execute(shell_string)
+
+class Command(BaseCommand):
+    '''
+    classdocs
+    '''
+    def add_arguments(self, parser):
+        '''
+        Adds the sum argument for this command, of course this will change in
+        the final implementation.
+        '''
+        parser.add_argument('--path', nargs=1, help='This is a stub for the, \
+            change detection command, right now all it does is sum numbers in \
+            the list.')
+        parser.add_argument('--output', nargs='*')
+
+    def handle(self, **options):
+        '''
+        In this example command, the values that come from the user input are
+        added up and the result is printed in the screen.
+        '''
+        output = options['output'][0]
+        zip = options['path'][0]
+        basename = get_base_name(zip)
+        aux_name = create_file_name(output, 'aux_%s' % basename)
+        real_name = create_file_name(output, basename)
+        with ZipFile(zip, 'r') as unzipped:   
+            unzipped.extractall(create_file_name(output, 'aux_%s' % basename))
+        path = create_file_name(aux_name, 'trend_tiles')
+        tifs =  get_contents_from_folder(path)
+        create_directory_path(real_name)
+        for tif in tifs:
+            source = create_file_name(path, tif)
+            target = create_file_name(real_name, tif)    
+            tile_map(source, target, 4000, 4000, 2, 2)
+        remove_directory(aux_name)
+        
+                      
+        

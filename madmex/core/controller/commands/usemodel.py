de5c0c4d3065ca233500a24f19314878288a23b4
amaurs <amaury.gtz@gmail.com>
2016-11-30 00:22:32 -0600

    Commands to train and use models on rapideye scenes.

--- madmex/core/controller/commands/usemodel.py
+++ madmex/core/controller/commands/usemodel.py
@@ -15,6 +15,7 @@ from sklearn.externals import joblib
 
 from madmex import _
 from madmex.core.controller.base import BaseCommand
+from madmex.core.controller.commands.createmodel import load_model
 from madmex.core.controller.commands.indexes import open_handle
 from madmex.core.controller.commands.ingest import _get_bundle_from_path
 from madmex.mapper.data._gdal import create_raster_from_reference
@@ -36,7 +37,10 @@ class Command(BaseCommand):
         parser.add_argument('--output', nargs=1, help='This is a stub for the, \
             change detection command, right now all it does is sum numbers in \
             the list.')
-        parser.add_argument('--model', nargs=1, help='This is a stub for the, \
+        parser.add_argument('--modelname', nargs='*', help='This is a stub for the, \
+            change detection command, right now all it does is sum numbers in \
+            the list.')
+        parser.add_argument('--modeldir', nargs=1, help='This is a stub for the, \
             change detection command, right now all it does is sum numbers in \
             the list.')
 
@@ -46,20 +50,28 @@ class Command(BaseCommand):
         added up and the result is printed in the screen.
         '''
         output = options['output'][0]
-        model_path = options['model'][0]
-        model = joblib.load(model_path)
-        for path in options['path']:
-            image_array = open_handle(path)
-            basename = get_base_name(path)[:7]
-            warnings.filterwarnings('ignore')
-            final = numpy.zeros((5000,5000))
-            import time
-            start_time = time.time()
-            for i in range(5000):
-                step = numpy.transpose(image_array[:,i,:])
-                final[i] = model.predict(step)
-                if i % 100 == 0:
-                    print i
-            print("--- %s seconds ---" % (time.time() - start_time))
-            classification = create_file_name(output, '%s.tif' % basename)
-            create_raster_from_reference(classification, final.reshape(5000, 5000), path, data_type=gdal.GDT_Byte, creating_options=['COMPRESS=LZW'])
+        models = options['modelname']
+        model_directory = options['modeldir'][0]
+        
+        for model_name in models:
+            persistence_directory = create_file_name(model_directory, model_name)
+            model = load_model(model_name)
+            model_instance = model.Model(persistence_directory)
+            model_instance.load(persistence_directory)
+            for path in options['path']:
+                image_array = open_handle(path)
+                x = image_array.shape[1]
+                y = image_array.shape[2]
+                basename = get_base_name(path)[:7]
+                warnings.filterwarnings('ignore')
+                final = numpy.zeros((x,y))
+                import time
+                start_time = time.time()
+                for i in range(x):
+                    step = numpy.transpose(image_array[:,i,:])
+                    final[i] = model_instance.predict(step)
+                    if i % 100 == 0:
+                        print i
+                print("--- %s seconds ---" % (time.time() - start_time))
+                classification = create_file_name(output, '%s-%s.tif' % (basename, model_name))
+                create_raster_from_reference(classification, final.reshape(5000, 5000), path, data_type=gdal.GDT_Byte, creating_options=['COMPRESS=LZW'])

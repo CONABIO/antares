55fc887dcc0a24beb14f6746ae0cb4f97572b220
amaurs <amaury.gtz@gmail.com>
2016-11-30 00:34:58 -0600

    Added comments to the supervised package.

--- madmex/model/supervised/c5.py
+++ madmex/model/supervised/c5.py
@@ -5,6 +5,8 @@ Created on Nov 25, 2016
 '''
 from __future__ import unicode_literals
 
+import logging
+
 import numpy
 import pandas
 
@@ -13,16 +15,28 @@ from madmex.remote.dispatcher import LocalProcessLauncher
 from madmex.util import create_file_name
 
 
+LOGGER = logging.getLogger(__name__)
+
 def save_to_file(data, filename):
+    '''
+    This method creates a file that will use the train method.
+    '''
     dataframe = pandas.DataFrame(data)
     dataframe.to_csv(filename, sep=str(','), encoding='utf-8', index = False, header = False)
-    
+
 def save_to_file_cases(data, filename):
+    '''
+    This method creates a file that will use the predict method.
+    '''
     dataframe = pandas.DataFrame(data)
     dataframe['given'] = '?'
     dataframe.to_csv(filename, sep=str(','), encoding='utf-8', index = False, header = False)
-    
+
 def create_names_file(target, bands, filename):
+    '''
+    This method creates a file that defines the features that will be used in the training
+    stage.
+    '''
     with open(filename,'w') as names:
         names.write('target.\n')
         for band in range(bands):
@@ -39,22 +53,30 @@ class Model(BaseModel):
         Constructor
         '''
         self.path = path
-        self.model_name = 'test'
+        self.model_name = 'c5'
 
     def fit(self, X, y):
+        '''
+        This method creates the necessary files to train a c5 model with the
+        c5.0 executable. It creates only the names and data files.
+        '''
         train_data = numpy.column_stack((X, y))
         save_to_file(train_data, create_file_name(self.path, '%s.data' % self.model_name))
         create_names_file(numpy.unique(y), X.shape[1],create_file_name(self.path, '%s.names' % self.model_name))
         launcher = LocalProcessLauncher()
-        shell_string = 'docker run -v %s:/results madmex/c5_execution c5.0 -f /results/%s' % (self.path, self.model_name)
-        print shell_string
+        shell_string = 'docker run --rm -v %s:/results madmex/c5_execution c5.0 -f /results/%s' % (self.path, self.model_name)
+        LOGGER.debug('Docker command: %s', shell_string)
         launcher.execute(shell_string)
 
     def predict(self, X):
+        '''
+        The c5 model creates a file with the tree extension and it
+        will be loaded by the predict executable.
+        '''
         save_to_file_cases(X, create_file_name(self.path, '%s.cases' % self.model_name))
         local = LocalProcessLauncher()
-        shell_string = 'docker run -v %s:/results madmex/c5_execution predict -f /results/%s' % (self.path, self.model_name)
-        print shell_string        
+        shell_string = 'docker run --rm -v %s:/results madmex/c5_execution predict -f /results/%s' % (self.path, self.model_name)
+        LOGGER.debug('Docker command: %s', shell_string)     
         output = local.execute(shell_string)
         by_line = output.split('\n')
         y = []
@@ -65,6 +87,14 @@ class Model(BaseModel):
         return numpy.array(y)
 
     def save(self, filepath):
+        '''
+        This method will do nothing in the case of a C5 model. The model is persisted
+        by default.
+        '''
         pass
+
     def load(self, filepath):
+        '''
+        This methid will do nothing in the case of a C5 model.
+        '''
         pass
--- madmex/model/supervised/rf.py
+++ madmex/model/supervised/rf.py
@@ -9,6 +9,7 @@ from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.externals import joblib
 
 from madmex.model.base import BaseModel
+from madmex.util import create_file_name
 
 class Model(BaseModel):
     '''
@@ -21,6 +22,8 @@ class Model(BaseModel):
         '''
         self.path = path
         self.model = RandomForestClassifier(n_estimators=200,n_jobs=20)
+        self.model_name = 'rf'
+
     def fit(self, X, y):
         self.model.fit(X,y)
 
@@ -34,13 +37,14 @@ class Model(BaseModel):
         '''
         Persists the trained model to a file.
         '''
-        joblib.dump(self.model, filepath) 
+        joblib.dump(self.model, create_file_name(filepath,'%s.pkl' % self.model_name)) 
 
     def load(self, filepath):
         '''
         Loads an already train model from a file to perform predictions.
         '''
-        self.model = joblib.load(filepath)
+        self.model = joblib.load(create_file_name(filepath,'%s.pkl' % self.model_name))
+
     def score(self, X, y):
         '''
         Lets the user load a previously trained model to predict with it. 

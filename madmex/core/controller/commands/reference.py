ced37cbfb2f392761202f6bfbeb630a3c321a3cf
amaurs <amaury.gtz@gmail.com>
2017-02-03 21:31:47 -0600

    The reference image now excludes 0 data.

--- madmex/core/controller/commands/reference.py
+++ madmex/core/controller/commands/reference.py
@@ -5,11 +5,13 @@ Created on Oct 8, 2015
 '''
 from __future__ import unicode_literals
 
+import numpy
+
 from madmex import _, util
 from madmex.core.controller.base import BaseCommand
 from madmex.mapper.data.raster import create_raster_tiff_from_reference
 from madmex.preprocessing.maskingwithreference import get_images_for_tile, \
-    create_reference_array
+    create_reference_array, create_stacked_array_rapideye
 
 
 class Command(BaseCommand):
@@ -53,6 +55,19 @@ class Command(BaseCommand):
                 create_raster_tiff_from_reference(re_raster_metadata, '%s.tif' % tile, reference_array)
         if paths:
             new_paths = map(util.get_parent, paths)
+            reference_array = numpy.sort(create_stacked_array_rapideye(new_paths), axis=0)
+            
+            
+            medians = numpy.empty((reference_array.shape[1], reference_array.shape[2], reference_array.shape[3]))
+            limit = reference_array.shape[1] / 5
+            
+            for band in range(reference_array.shape[1]):
+                array_band = numpy.ma.array(reference_array[:, band, :, :], mask = reference_array[:, band, :, :]==0)
+                medians[band] = numpy.ma.median(array_band, axis=0)
+            
             bundle = Bundle(new_paths[0])
             re_raster_metadata = bundle.get_raster().metadata
-            create_raster_tiff_from_reference(re_raster_metadata, '%s.tif' % name, reference_array)
+            create_raster_tiff_from_reference(re_raster_metadata, '%s_max.tif' % name, reference_array[0])
+            create_raster_tiff_from_reference(re_raster_metadata, '%s_mean.tif' % name, reference_array[8])
+            create_raster_tiff_from_reference(re_raster_metadata, '%s_median.tif' % name, medians)
+            create_raster_tiff_from_reference(re_raster_metadata, '%s_min.tif' % name, reference_array[reference_array.shape[0] - 1])

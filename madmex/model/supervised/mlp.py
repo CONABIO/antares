31a342b885d8b74984f1f704335ebd84ddc5d874
robmartz <rmartinez@conabio.gob.mx>
2017-02-10 13:29:17 -0600

    Save scaled models in MLPClassifier and Support Vec. Machine

--- madmex/model/supervised/mlp.py
+++ madmex/model/supervised/mlp.py
@@ -6,6 +6,7 @@ Created on Feb 2, 2017
 from __future__ import unicode_literals
 
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.externals import joblib
 from madmex.model.base import BaseModel
 from madmex.util import create_file_name
@@ -22,30 +23,48 @@ class Model(BaseModel):
         Constructor
         '''
         self.path = path
-        self.model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+        self.model = MLPClassifier(solver='lbfgs', max_iter=600, hidden_layer_sizes=(300,), random_state=1)
+        #self.model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(200,7), random_state=1)
+        #self.model = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+        #                           beta_1=0.9, beta_2=0.999, early_stopping=False,
+        #                           epsilon=1e-08, hidden_layer_sizes=(200,), learning_rate='constant',
+        #                           learning_rate_init=0.001, max_iter=300, momentum=0.9,
+        #                           nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+        #                           solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
+        #                           warm_start=False)
         
-        self.model_name = 'mpl'
-
+        self.model_name = 'mlp'
+        self.scaler_mlp = 'scaler'
+        self.scaler = StandardScaler()
+        
+        
     def fit(self, X, y):
-        self.model.fit(X,y)
+        self.scaler.fit(X)
+        X_train = self.scaler.transform(X)
+        self.model.fit(X_train,y)
 
     def predict(self, X):
         '''
         Simply passes down the prediction from the underlying model.
         '''
-        return self.model.predict(X)
+        X_train = self.scaler.transform(X)
+        return self.model.predict(X_train)
 
     def save(self, filepath):
         '''
         Persists the trained model to a file.
         '''
         joblib.dump(self.model, create_file_name(filepath,'%s.pkl' % self.model_name)) 
+        joblib.dump(self.scaler, create_file_name(filepath,'%s.pkl' % self.scaler_mlp)) 
+
 
     def load(self, filepath):
         '''
         Loads an already train model from a file to perform predictions.
         '''
         self.model = joblib.load(create_file_name(filepath,'%s.pkl' % self.model_name))
+        self.scaler = joblib.load(create_file_name(filepath,'%s.pkl' % self.scaler_mlp))
+
 
     def score(self, X, y):
         '''

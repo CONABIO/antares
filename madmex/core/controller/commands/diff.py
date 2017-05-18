282f8504409f8ed3783fed47fb538f3631dde7cc
amaurs <amaury.gtz@gmail.com>
2017-05-18 15:18:28 -0500

    Script to process Hansen products.

--- madmex/core/controller/commands/diff.py
+++ madmex/core/controller/commands/diff.py
@@ -38,47 +38,60 @@ class Command(BaseCommand):
         added up and the result is printed in the screen.
         '''
         sum_of_numbers = 0
-        path_a = options['a'][0]
-        path_b = options['b'][0]
+        #path_a = options['a'][0]
+        #path_b = options['b'][0]
         
         init = 1985
         gdal_format = 'GTiff'
-        for state in ['Campeche','Chiapas','Oaxaca','Quintana_Roo','Yucatan']:
-            for where in ['anps', 'corr-anp', 'est-corr-anp']:
-                for i in range(30):
-                    path_a = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/corte_por_area_interes_en_estado/%s/%s/Mexico_TCC_%s_%s_%s.tif' % (state, init+i, state,init+i, where)
-                    path_b = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/corte_por_area_interes_en_estado/%s/%s/Mexico_TCC_%s_%s_%s.tif' % (state, init+i+1, state,init+i+1, where)
-                    path_c = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/corte_por_area_interes_en_estado/diff/Mexico_TCC_%s_%s_%s_%s.tif' % (state, init+i,init+i+1,where)
-                    
-                    print path_a
-                    print path_b
-                    print path_c
-                    
-                    #remove_file(path_c)
-                    
-                    if not check_if_file_exists(path_c):
-                    
-                        image_a = raster.Data(path_a, gdal_format)
-                        image_b = raster.Data(path_b, gdal_format)
-                    
-                        array_a = image_a.read_data_file_as_array()
-                        array_b = image_b.read_data_file_as_array()
-                    
-                        diff = array_b - array_a
-                    
-                        '''
-                        absolute = numpy.absolute(diff)
-                        absolute[absolute < 30] = 0
-                        absolute[absolute != 0] = 1
-                        absolute[diff < 0] *= 2
-                        '''
-                    
-                        create_raster_from_reference(path_c, diff, path_a, data_type=gdal.GDT_Int16)
-                    else:
-                        print 'File exists.'
-                    
-                    print 'Done %s %s-%s' % (where, init+i,init+i)
-        
+        for i in range(30):
+            path_a = '/LUSTRE/MADMEX/tasks/2016_tasks/matt_hansen_forests/final/Mexico_TCC_%s.vrt' % (init+i)
+            path_b = '/LUSTRE/MADMEX/tasks/2016_tasks/matt_hansen_forests/final/Mexico_TCC_%s.vrt' % (init+i+1)
+            path_c = '/LUSTRE/MADMEX/tasks/2017_tasks/conafor_entrega_junio/tcc/Mexico_TCC_%s_%s.tif' % (init+i,init+i+1)
+            
+            print path_a
+            print path_b
+            print path_c
+            
+            #remove_file(path_c)
+            
+            if not check_if_file_exists(path_c):
+            
+                image_a = raster.Data(path_a, gdal_format)
+                image_b = raster.Data(path_b, gdal_format)
+            
+                array_a = image_a.read_data_file_as_array()
+                array_b = image_b.read_data_file_as_array()
+            
+                #mask = ((array_a < 30) | (70 < array_a)) & ((array_b < 30) | (70 < array_b))
+                
+                #array_a[array_a <= 30] = 0
+                #array_a[30 < array_a] = 1
+                
+                #array_b[array_b <= 30] = 0
+                #array_b[30 < array_b] = 1 
+                
+                #diff = array_b - array_a  
+                
+                
+                diff = numpy.zeros(numpy.shape(array_a))                     
+                
+                diff[((array_b - array_a) < 10) & (array_a >= 30)] = 1
+                
+                #diff[mask] = -9999
+            
+                '''
+                absolute = numpy.absolute(diff)
+                absolute[absolute < 30] = 0
+                absolute[absolute != 0] = 1
+                absolute[diff < 0] *= 2
+                '''
+            
+                create_raster_from_reference(path_c, diff, path_a, data_type=gdal.GDT_Int16)
+            else:
+                print 'File exists.'
+            
+            print 'Done %s-%s' % (init+i,init+i)
+        '''
         
         total = {}
         for state in ['Campeche','Chiapas','Oaxaca','Quintana_Roo','Yucatan']:
@@ -114,54 +127,61 @@ class Command(BaseCommand):
                     mask_image = raster.Data(mask_path, gdal_format)
                     mask_array = mask_image.read_data_file_as_array()
                 
-                    counts = numpy.unique(mask_array, return_counts=True)
                     
-                    print counts
+                    
+                    
         
-                    path_c = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/corte_por_area_interes_en_estado/diff/Mexico_TCC_%s_%s_%s_%s.tif' % (state, init+i,init+i+1,where)
+                    path_c = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/corte_por_area_interes_en_estado/diff-michael-repro/Mexico_TCC_%s_%s_%s_%s.tif' % (state, init+i,init+i+1,where)
                     
                     print path_c
                     
                     image_c = raster.Data(path_c, gdal_format)
                     
                     array_c = image_c.read_data_file_as_array()
-                    array_c = array_c.flatten()
+                    #array_c = array_c[array_c > -9999]
+                    
+                    counts = numpy.unique(array_c, return_counts=True)
+                    print counts
+                    
+                    
+                    #size_non_zero = len(array_c)
+                    #final = numpy.zeros(size_non_zero, dtype=numpy.int)
+                    #final[(-40 < array_c) & (array_c < 40)] = 0
+                    #final[40 < array_c] = 1
+                    #final[array_c < -40] = 2
+                    
+                    
                     x_resolution = image_c.get_geotransform()[1]
                     y_resolution = image_c.get_geotransform()[5]
                     
                     area = x_resolution * y_resolution
                     
-                    print "len", len(array_c)
-                    array_c = array_c[numpy.nonzero(array_c)]
-                    print "len", len(array_c)
-                    counts = numpy.unique(array_c, return_counts=True)
-                    
-                    print counts
-                    
-                    
+                
                     stats = {}
                     
                     stats['period'] = '%s-%s' % (init+i,init+i+1)
                     stats['type'] = where
                     stats['resolution'] = area
-                    if len(array_c) > 0:
-                        stats['positive'] = numpy.percentile(array_c, 70)
-                        print numpy.percentile(array_c, 70)
-                        stats['negative'] = numpy.percentile(array_c, 30)
-                        print numpy.percentile(array_c, 30)
+                    
+                    if len(counts[1]) > 2:
+                        stats['negative'] = counts[1][0]
+                        stats['no-change'] = counts[1][1]
+                        stats['positive'] = counts[1][2]
                     else:
-                        stats['positive'] = 0.0
-                        stats['negative'] = 0.0
+                        stats['negative'] = 0
+                        stats['no-change'] = counts[1][0]
+                        stats['positive'] = 0
+                     
                     #stats['net'] = (counts[1][1] - counts[1][2])
                     stats['total'] = total[state][where]
                     statistics_by_state.append(stats)
                     print statistics_by_state
             
-            stats_path = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/shapes_area_interes_por_estado/stats/estadisticas-%s.json' % state
+            stats_path = '/LUSTRE/MADMEX/staging/2017_tasks/corredor_biologico/analisis_por_estado-vegetacion/shapes_area_interes_por_estado/stats/estadisticas-michael-repro-%s.json' % state
             
             import json
             with open(stats_path, 'w') as outfile:
                 json.dump(statistics_by_state, outfile)
         
-        
+        '''
         

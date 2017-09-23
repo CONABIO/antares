646420e2d8ba3c8ebf4cfbf8a0659f668c3db045
Metaphisikz <julian.equihua@gmail.com>
2017-09-23 18:07:45 -0500

    antares is the worst, fixed non-integer indices when reading raster data as array

--- madmex/mapper/data/harmonized.py
+++ madmex/mapper/data/harmonized.py
@@ -277,13 +277,13 @@ class Data(BaseData):
         xoffset = self.get_attribute(XOFFSET)
         x_range = self.get_attribute(XRANGE)  
         y_range = self.get_attribute(YRANGE)
-        #try:
-        image1_data_array = image1_data_class.read_data_file_as_array()[:, int(yoffset[0]):int(yoffset[0] + y_range), int(xoffset[0]):int(xoffset[0] + x_range)]
-        image2_data_array = image2_data_class.read_data_file_as_array()[:, int(yoffset[1]):int(yoffset[1] + y_range), int(xoffset[1]):int(xoffset[1] + x_range)]
-        return (image1_data_array, image2_data_array) 
-        #except Exception:
-        #    LOGGER.error('Process of harmonize arrays failed, one image is %s', image1_data_class.image_path)
-        #    print('Process of harmonize arrays failed, one image is %s', image1_data_class.image_path)
+        try:
+            image1_data_array = image1_data_class.read_data_file_as_array()[:, int(yoffset[0]):int(yoffset[0] + y_range), int(xoffset[0]):int(xoffset[0] + x_range)]
+            image2_data_array = image2_data_class.read_data_file_as_array()[:, int(yoffset[1]):int(yoffset[1] + y_range), int(xoffset[1]):int(xoffset[1] + x_range)]
+            return (image1_data_array, image2_data_array) 
+        except Exception:
+            LOGGER.error('Process of harmonize arrays failed, one image is %s', image1_data_class.image_path)
+            print('Process of harmonize arrays failed, one image is %s', image1_data_class.image_path)
     def get_attribute(self, path_to_attribute):
         '''
         Returns the attribute that is found in the given path.

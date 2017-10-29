cd3e6713542aa8bc555b1bfa29002806fddba7e9
amaurs <amaury.gtz@gmail.com>
2017-10-28 20:42:34 -0500

    Finished implementation of usgs api client.

--- madmex/management/commands/downloadorder.py
+++ madmex/management/commands/downloadorder.py
@@ -7,10 +7,18 @@ import json
 import logging
 
 from madmex.management.base import AntaresBaseCommand
-from madmex.remote import UsgsApi
+from madmex.models import LandsatCatalog
+from madmex.remote import UsgsApi, maybe_download_and_extract
 
 
 logger = logging.getLogger(__name__)
+
+images = ['LC08_L1TP_026047_20171022_20171022_01_RT',
+          'LC08_L1TP_026047_20171006_20171023_01_T1',
+          'LC08_L1TP_026047_20170920_20171012_01_T1',
+          'LC08_L1TP_026047_20170904_20170916_01_T1',
+          'LC08_L1TP_026047_20170819_20170826_01_T1']
+
 
 class Command(AntaresBaseCommand):
     '''
@@ -25,11 +33,37 @@ class Command(AntaresBaseCommand):
         
         api = UsgsApi()
         
-        api.get_functions()
+        data = api.get_functions()
         
-        data = api.get_resampling_methods()
-        #api.list_order(order_id)
+        row = 26
+        path = 47
         
         
-        print json.dumps(data, indent=4)
+        '''
+        data = api.get_available_products(images[0])
+        products = data['olitirs8_collection']['products']
+        data = api.order(images, products)
+        '''
+        
+        order_id = "espa-amaury.gtz@gmail.com-10282017-193609-629"
+        
+        data = api.get_list_order(order_id)
+        #print json.dumps(data, indent=4)
+        
+        for scene in data[order_id]:
+            print scene['product_dload_url']
+            maybe_download_and_extract('/Users/agutierrez/Documents/scene026036', scene['product_dload_url'])
+        
+        #maybe_download_and_extract('/Users/agutierrez/Documents/scene026036', scene['product_dload_url'])
+        #data = api._consume_api('/v0')
+        #print json.dumps(data, indent=4)
+        #data = api.get_available_products('LC08_L1TP_029030_20161008_20170220_01_T1')
+        
+        
+        #data = api.get_user_info()
+        
+        #api.get_list_order(order_id)
+        
+        
+        #print json.dumps(data, indent=4)
         
--- madmex/remote/__init__.py
+++ madmex/remote/__init__.py
@@ -15,7 +15,11 @@ from web.settings import USGS_USER, USGS_PASSWORD
 logger = logging.getLogger(__name__)
 
 def maybe_download_and_extract(target_directory, scene_url):
-    
+    '''
+    This method will try to download the requested url, if 
+    a file with the url name has already been downloaded it
+    will not proceed.
+    '''
     filename = scene_url.split('/')[-1]
     filepath = create_file_name(target_directory, filename)
     if not is_file(filepath):
@@ -35,62 +39,106 @@ def maybe_download_and_extract(target_directory, scene_url):
 
 class UsgsApi():
     def __init__(self):
+        '''
+        This is the constructor, it creates an object that holds
+        credentials to usgs portal. 
+        '''
         if USGS_USER != None and USGS_PASSWORD != None:
-            
             self.username = USGS_USER
             self.password = USGS_PASSWORD
             self.host = 'https://espa.cr.usgs.gov'
         else:
             logger.error('Please add the usgs credentials to the .env file.')
             sys.exit(-1)
-        
-    def _consume_api(self, endpoint, arg=None):
+
+    def _consume_api(self, endpoint, data=None):
+        '''
+        This method hides the complexity of making a request to usgs,
+        depending on whether data parameter is given or not, it makes
+        a GET or a POST request. It requires an endpoint to query the
+        api if an invalid request is given, then the aip will answer
+        with a 404 error message.
+        '''
         raw = "%s:%s" % (USGS_USER, USGS_PASSWORD)
         authorization = 'Basic %s' % base64.b64encode(raw).strip()
         url = self.host + '/api' + endpoint
-        request = urllib.request.Request(url)
+        if not data:
+            request = urllib.request.Request(url)
+        else: # a POST request
+            request = urllib.request.Request(url, data=data,
+                             headers={'content-type': 'application/json'})
         logger.info(url)
         request.add_header('Authorization', authorization)
         response = urllib.request.urlopen(request)
         data = json.load(response.fp)
         return data
+
     def get_functions(self):
-        data = self._consume_api('/v1')
-        for key, value in data['operations'].iteritems():
-            print key
-        
+        '''
+        Will query the api for the valid functions that it exposes.
+        '''
+        return self._consume_api('/v1')
+
     def get_projections(self):
+        '''
+        Returns the available projections that the api supports.
+        '''
         return self._consume_api('/v1/projections')
-    
-    def get_available_products(self):
-        return self._consume_api('/v1/available-products')
+
+    def get_available_products(self, scene_id):
+        '''
+        Given a scene id, this method returns the available products
+        that can be requested in an order.
+        '''
+        return self._consume_api('/v1/available-products/%s' % scene_id)
     
     def get_formats(self):
+        '''
+        Returns the available formats in which the data can be requested.
+        '''
         return self._consume_api('/v1/formats')
     
-    def get_orders(self):
-        return self._consume_api('/v1/order')
+    def order(self, inputs, products):
+        '''
+        This is the only method implementing a post request. Data about which
+        scenes are requested and which products are needed is required on 
+        this method calling. More complex querying is supported, we are keeping
+        this simple for now.
+        '''
+        request_json = {'format':'gtiff',
+                        'note':'testing',
+                        'olitirs8_collection':{
+                                'inputs':inputs,
+                                'products':products
+                            }
+                        } 
+        payload = json.dumps(request_json).encode('utf8')
+        return self._consume_api('/v1/order', payload)
     
     def get_list_orders(self):
+        '''
+        This methos returns the status of the orders that have been posted.
+        '''
         return self._consume_api('/v1/list-orders')
+
     def get_resampling_methods(self):
+        '''
+        Returns the resampling methods available when the products are
+        requested in a different resolution that the standard one. Default
+        value will be nearest neighbors.
+        '''
         return self._consume_api('/v1/resampling-methods')
-        
-    def list_order(self, order_id):
-        raw = "%s:%s" % (USGS_USER, USGS_PASSWORD)
-        authorization = 'Basic %s' % base64.b64encode(raw).strip()
-        url = self.host + '/api/v1/item-status/' + order_id
-        logger.debug('Retrieving order from %s.' % url)
-        request = urllib.request.Request(url)
-        request.add_header('Authorization', authorization)
-        response = urllib.request.urlopen(request)
-        data = json.load(response.fp)
-        
-        for scene in data[order_id]:
-            if scene['status'] == 'complete':
-                pass
-                #maybe_download_and_extract('/Users/agutierrez/Documents/scene026036', scene['product_dload_url'])
-           
-        print json.dumps(data, indent=4, sort_keys=True)
-        
-        
+
+    def get_user_info(self):
+        '''
+        Returns information about the user that the credentials that this
+        object holds.
+        '''
+        return self._consume_api('/v1/user')
+
+    def get_list_order(self, order_id):
+        '''
+        Returns the status of the given order.
+        '''
+        url = '/v1/item-status/%s' % order_id
+        return self._consume_api(url)

60f98c847e9e8bb50cb64f781d278ddba1de6632
amaurs <amaury.gtz@gmail.com>
2017-11-30 19:54:47 -0600

    Some refactoring.

--- madmex/admin.py
+++ madmex/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from django.contrib.gis import admin
 
-from madmex.models import WorldBorder
+from madmex.models import WorldBorder, Segment
 
 
-admin.site.register(WorldBorder, admin.GeoModelAdmin)
+admin.site.register(Segment, admin.GeoModelAdmin)
--- madmex/management/commands/segment.py
+++ madmex/management/commands/segment.py
@@ -30,6 +30,44 @@ worldborder_mapping = {
             'mpoly' : 'MULTIPOLYGON',
             }
 
+def slic_segmentation(image_path, avg_segment_size_ha=5, compactness=0.01):
+    with rasterio.open(image_path) as src:
+        meta = src.meta
+        transform = src.transform
+        img = np.empty((src.height, src.width, src.count),
+                    dtype=src.meta['dtype'])
+        for layer_id in range(src.count):
+            img[:,:,layer_id] = src.read(layer_id + 1)
+            area_ha = (src.res[0] ** 2 * src.width * src.height) / 10000
+    n_segments = int(floor(area_ha / avg_segment_size_ha))
+    segments = segmentation.slic(img, 
+                                 compactness = compactness,
+                                 n_segments=n_segments, 
+                                 multichannel = True,
+                                 enforce_connectivity=True)
+    
+    return segments, transform, meta
+
+def persist_database(shapes):
+    for item in shapes:
+        s = json.dumps(item[0])
+        g1 = geojson.loads(s)
+        g2 = shape(g1)
+        myGeom = GEOSGeometry(g2.wkt)
+        seg = Segment(segment_id = int(item[1]),
+                      mpoly = geos.MultiPolygon(myGeom))
+        seg.save()
+
+def persist_file(shapes, output, meta):
+    fiona_kwargs = {'crs': meta['crs'],
+                    'driver': 'GPKG',
+                    'schema': {'geometry': 'Polygon',
+                    'properties': {'id': 'int'}}}
+    
+    with fiona.open(output, 'w', layer='slic', **fiona_kwargs) as dst:
+        for item in shapes:
+            feature = {'geometry': item[0], 'properties': {'id': int(item[1])}}
+            dst.write(feature)
 class Command(AntaresBaseCommand):
     '''
     classdocs
@@ -47,75 +85,11 @@ class Command(AntaresBaseCommand):
         added up and the result is printed in the screen.
         '''
         stack = options['path'][0]
-        
-        
-
         output_vector_file = '/Users/agutierrez/Desktop/Caribe/%s.gpkg' % get_basename(stack)
-
-
-        print output_vector_file
-
-        # Read multilayer raster to 3D array. Dimensions order must be x,y,z
-        with rasterio.open(stack) as src:
-            meta = src.meta
-            transform = src.transform
-            img = np.empty((src.height, src.width, src.count),
-                    dtype=src.meta['dtype'])
-            for layer_id in range(src.count):
-                img[:,:,layer_id] = src.read(layer_id + 1)
-                area_ha = (src.res[0] ** 2 * src.width * src.height) / 10000
-
-
-
-        ## Slick parameters
-        avg_segment_size_ha = 5
-        compactnesses = [0.01]
-        n_segments = int(floor(area_ha / avg_segment_size_ha))
-        # 
-        for compactness in compactnesses:
-            segments = segmentation.slic(img, compactness = compactness,
-                                 n_segments=n_segments, multichannel = True,
-                                enforce_connectivity=True)
-            # Vectorize
-            shapes = features.shapes(segments.astype(np.uint16), transform=transform)
+        segments, transform, meta = slic_segmentation(stack)
             
+        # Vectorize
+        shapes = features.shapes(segments.astype(np.uint16), transform=transform)
             
-            
-
-            # Write to file
-            for item in shapes:
-                feature = {'geometry': item[0], 'properties': {'id': int(item[1])}}
-                    
-                #print json.dumps(item[0])
-                    
-                    
-                s = json.dumps(item[0])
-                g1 = geojson.loads(s)
-                g2 = shape(g1)
-                    
-                #epsg = meta['crs']['init'].split(':')[1]
-                #print epsg
-                myGeom = GEOSGeometry(g2.wkt)
-                    
-                #gcoord = SpatialReference(4326)
-                #mycoord = SpatialReference(int(epsg))
-                
-                #trans = CoordTransform(mycoord, gcoord)
-
-                    
-                seg = Segment(segment_id = int(item[1]),
-                                  mpoly = geos.MultiPolygon(myGeom))
-                seg.save()
-            '''
-            # Prepare schema
-            fiona_kwargs = {'crs': meta['crs'],
-                    'driver': 'GPKG',
-                    'schema': {'geometry': 'Polygon',
-                    'properties': {'id': 'int'}}}
-            with fiona.open(output_vector_file, 'w', layer='slic_%3f_%d' % (compactness, n_segments),
-                    **fiona_kwargs) as dst:
-                for item in shapes:
-                    feature = {'geometry': item[0], 'properties': {'id': int(item[1])}}
-                                        
-                    dst.write(feature)
-            '''
+        #persist_database(shapes)
+        persist_file(shapes, output_vector_file, meta)
